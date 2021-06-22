d6b60563793a950b9469787d9ad53c9f15e9031d
Martin Fuster <mfuster@litebox.ai>
2021-06-22 17:01:00 -0300

    navbar completed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import * as SC from "./styles";
+import { Container, Button } from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import client from "../../assets/client.jpg";
 
 const Navbar: React.FC = () => {
     return (
         <SC.Background>
-as
+            <Container maxWidth="lg" >
+                <SC.Nav>
+                    <SC.CompanyImg />
+                    <SC.CompanyName>ACME CORP</SC.CompanyName>
+                    <SC.Arrow />
+                    <SC.SectionName>Connectors</SC.SectionName>
+                    <SC.LinksContainer>
+                        <SC.Link href="/support">Support</SC.Link>
+                        <SC.Link href="/docs">Docs</SC.Link>
+                    </SC.LinksContainer>
+                    <SC.Button>
+                        <Button size="large" startIcon={<SC.User src={client} />} endIcon={<ExpandMoreIcon />} variant="outlined" color="inherit">
+                            Stage
+                        </Button>
+                    </SC.Button>
+                </SC.Nav>
+            </Container>
         </SC.Background>
     )
 }
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import navbarBg from '../../assets/navbar.svg';
+import navbarBg from "../../assets/navbar.svg";
+import user from "../../assets/company-logo.svg";
+import arrow from "../../assets/right-arrow-nav.svg";
 
 export const Background = styled.div`
     width: 100vw;
@@ -8,4 +10,69 @@ export const Background = styled.div`
     background-image: url(${navbarBg});
     background-repeat: no-repeat;
     background-size: cover;
+    padding-top: 67px;
+`;
+
+export const Nav = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+`;
+
+export const CompanyImg = styled.div`
+    height: 56px;
+    width: 56px;
+    background-size: contain;
+    background-image: url(${user});
+    margin-right: 24px;
+`;
+
+export const CompanyName = styled.h3`
+    font-size: 20px;
+    line-height: 22px;
+    font-weight: 600;
+    color: #FF6422;
+    margin-right: 8px;
+`;
+
+export const Arrow = styled.div`
+    height: 10.5px;
+    width: 8px;
+    margin-right: 16px;
+    background-size: contain;
+    background-image: url(${arrow});
+    background-repeat: no-repeat;
+`;
+
+export const SectionName = styled.h3`
+    font-size: 20px;
+    line-height: 22px;
+    font-weight: 600;
+    color: white;
+`;
+
+export const LinksContainer = styled.div`
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+`;
+
+export const Link = styled.a`
+    font-size: 14px;
+    line-height: 16px;
+    font-weight: 500;
+    text-decoration: none;
+    color: white;
+    margin-right: 72px;
+`;
+
+export const Button = styled.div`
+    color: white;
+`;
+
+export const User = styled.img`
+    height: 24px;
+    width: 24px;
+    border-radius: 50%;
+    object-fit: contain;
 `;
