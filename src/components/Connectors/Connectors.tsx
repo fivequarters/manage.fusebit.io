6abb0fb2941bc6b19ae3dcabcc565311665caa3b
Martin Fuster <mfuster@litebox.ai>
2021-07-14 13:21:13 -0300

    removed fatal error from connectors

--- src/components/Connectors/Connectors.tsx
+++ src/components/Connectors/Connectors.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import TabComponent from "../TabComponent";
-import FatalError from "../FatalError";
 import Overview from "./Overview";
 
 const Connectors: React.FC = () => {
     return (
-        // <TabComponent
-        // tabNames={["Overview", "Health", "Analytics"]}
-        // tabObjects={[<Overview />, "Health", "Analytics"]}
-        // />
-        <FatalError subtitle={"asd"} description={"asd"} />
+        <TabComponent
+        tabNames={["Overview", "Health", "Analytics"]}
+        tabObjects={[<Overview />, "Health", "Analytics"]}
+        />
     )
 }
 
