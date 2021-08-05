04c2419638411e906ec20ebd91cc23651449065e
Martin Fuster <mfuster@litebox.ai>
2021-08-05 18:19:38 -0300

    added identities to connectors tab component

--- src/components/Connectors/Connectors.tsx
+++ src/components/Connectors/Connectors.tsx
@@ -6,8 +6,8 @@ const Connectors: React.FC = () => {
     let headless = useRef(true); // the parent has to have this otherwise the mounting of the overview will open the feed picker if there is a query param.
     return (
         <TabComponent
-        tabNames={["Overview"]}
-        tabObjects={[<Overview headless={headless} setHeadless={(value: boolean) => headless.current = value} />]}
+        tabNames={["Overview", "Identities"]}
+        tabObjects={[<Overview headless={headless} setHeadless={(value: boolean) => headless.current = value} />, "Identities"]}
         />
     )
 }
