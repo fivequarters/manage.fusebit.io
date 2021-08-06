33c63bc79690f6bf59c4a4ec4db351e44b00c0c7
Martin Fuster <mfuster@litebox.ai>
2021-08-06 15:47:29 -0300

    identities logic and api calls completed

---
+++ src/hooks/api/v2/account/connector/identity/useDeleteOne.ts
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from 'react-query';
+import { Params } from '../../../../../../interfaces/api';
+import { useAxios } from '../../../../../useAxios';
+
+export const useAccountConnectorIdentityDeleteOne = <T>() => {
+    const { axios } = useAxios();
+    const queryClient = useQueryClient();
+
+    return useMutation((params: Params) => {
+        const { accountId, subscriptionId, id, ...data } = params;
+        return axios<T>(`/v2/account/${accountId}/subscription/${subscriptionId}/connector/${id}/identity/${data.id}`, 'delete');
+    }, {
+        onMutate: (_: Params) => () => {},
+        onError: (_, __, rollback) => rollback?.(),
+        onSuccess: () => queryClient.removeQueries('accountConnectorIdentityGetAll'),
+    });
+}
