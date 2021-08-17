7f1d5acccb9188443b811c98f7b3871b3ef9ff3e
Martin Fuster <mfuster@litebox.ai>
2021-08-17 12:44:03 -0300

    changed tab component to link to the strings

--- src/components/IntegrationDetail/IntegrationDetail.tsx
+++ src/components/IntegrationDetail/IntegrationDetail.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import TabComponent from '../TabComponent';
 import Develop from './Develop';
-import Installs from './Installs';
 
 const IntegrationDetail: React.FC = () => {
-  return <TabComponent tabNames={['Develop', 'Installs']} tabObjects={[<Develop />, <Installs />]} />;
+  return (
+    <TabComponent tabNames={['Develop', 'Installs']} tabObjects={[<Develop />, window.location.href + '/installs']} />
+  );
 };
 
 export default IntegrationDetail;
