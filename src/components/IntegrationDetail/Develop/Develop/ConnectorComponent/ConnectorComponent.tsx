ee7c70bce8eb73e25801c63ac60cc23f003fe92e
Martin Fuster <mfuster@litebox.ai>
2021-08-18 19:56:17 -0300

    on connector delete we now show a warning popup before allowing the user to delete the connector

--- src/components/IntegrationDetail/Develop/Develop/ConnectorComponent/ConnectorComponent.tsx
+++ src/components/IntegrationDetail/Develop/Develop/ConnectorComponent/ConnectorComponent.tsx
@@ -5,6 +5,7 @@ import cross from '../../../../../assets/cross.svg';
 import { useHistory } from 'react-router-dom';
 import { useGetRedirectLink } from '../../../../../hooks/useGetRedirectLink';
 import { integrationsFeed, connectorsFeed } from '../../../../../static/feed';
+import { Button, Modal, Backdrop } from '@material-ui/core';
 
 const ConnectorComponent: React.FC<ConnectorComponentProps> = ({
   connector,
@@ -15,6 +16,7 @@ const ConnectorComponent: React.FC<ConnectorComponentProps> = ({
   const history = useHistory();
   const { getRedirectLink } = useGetRedirectLink();
   const [icon, setIcon] = useState('');
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
   useEffect(() => {
     if (connector.tags && !connector.missing) {
@@ -48,11 +50,45 @@ const ConnectorComponent: React.FC<ConnectorComponentProps> = ({
       onClick={(e: any) => {
         if (linkConnector) {
           onLinkConnectorClick && onLinkConnectorClick(connector.id);
-        } else if (!e.target.id) {
+        } else if (!e.target.id && !deleteModalOpen) {
           history.push(getRedirectLink(`/connector/${connector.id}/configure`));
         }
       }}
     >
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={deleteModalOpen}
+        onClose={() => setDeleteModalOpen(false)}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+      >
+        <SC.Card open={deleteModalOpen}>
+          <SC.Close onClick={() => setDeleteModalOpen(false)} src={cross} alt="cross" height="12" width="12" />
+          <SC.Title>Are you sure you want to delete this connector?</SC.Title>
+          <SC.Description>You cannot undo this action.</SC.Description>
+          <SC.ButtonsWrapper>
+            <Button
+              onClick={() => setDeleteModalOpen(false)}
+              style={{ width: '77px', marginRight: '16px' }}
+              size="medium"
+              variant="outlined"
+              color="primary"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => onConnectorDelete(connector.id)}
+              style={{ width: '77px' }}
+              size="medium"
+              variant="contained"
+              color="primary"
+            >
+              Delete
+            </Button>
+          </SC.ButtonsWrapper>
+        </SC.Card>
+      </Modal>
       {icon === '' ? (
         <SC.CardConnectorImagePlaceholder />
       ) : (
@@ -62,7 +98,7 @@ const ConnectorComponent: React.FC<ConnectorComponentProps> = ({
         {connector.id} {connector.missing && 'is not found'}
       </SC.CardConnectorText>
       {!linkConnector && (
-        <SC.CardConnectorCrossContainer id="closeWrapper" onClick={() => onConnectorDelete(connector.id)}>
+        <SC.CardConnectorCrossContainer id="closeWrapper" onClick={() => setDeleteModalOpen(true)}>
           <SC.CardConnectorCross id="close" src={cross} alt="close" height="8" width="8" />
         </SC.CardConnectorCrossContainer>
       )}
--- src/components/IntegrationDetail/Develop/Develop/ConnectorComponent/styles.ts
+++ src/components/IntegrationDetail/Develop/Develop/ConnectorComponent/styles.ts
@@ -101,3 +101,76 @@ export const CardConnectorSeeMore = styled.a`
     cursor: pointer;
   }
 `;
+
+export const ModalWrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  outline: transparent;
+  padding: 64px;
+  background-color: white;
+  border-radius: 8px;
+`;
+
+export const Card = styled.div<{ open: boolean }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  opacity: ${(props) => (props.open ? 1 : 0)};
+  padding: 64px 100px;
+  width: 449px;
+  border-radius: 8px;
+  box-shadow: 0px 20px 48px rgba(52, 72, 123, 0.1);
+  outline: transparent;
+  transition: all 1s linear;
+
+  @media only screen and (max-width: 550px) {
+    width: 100%;
+    left: 0;
+    top: auto;
+    bottom: 0;
+    transform: translate(0, 0);
+  }
+`;
+
+export const Close = styled.img`
+  height: 12px;
+  width: 12px;
+  object-fit: contain;
+  position: absolute;
+  top: 32px;
+  right: 32px;
+  transition: all 0.25s linear;
+
+  &:hover {
+    cursor: pointer;
+    transform: rotate(90deg);
+  }
+`;
+
+export const Title = styled.h3`
+  font-size: 20px;
+  line-height: 26px;
+  font-weight: 600;
+  color: var(--black);
+  margin-bottom: 16px;
+  text-align: center;
+  max-width: 249px;
+`;
+
+export const Description = styled.p`
+  font-size: 14px;
+  line-height: 20px;
+  color: var(--black);
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
