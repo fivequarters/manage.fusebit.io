c26fcaa44728a63ae138a570d8c85fc2bf1b0d8d
Benn Bollay <bb.github@magitech.org>
2021-08-18 12:41:57 -0700

    NFC: Parameterize the S3_BUCKET to deploy the website into

--- scripts/publish_website.sh
+++ scripts/publish_website.sh
@@ -12,10 +12,10 @@ set -e
 
 aws --profile=${AWS_PROFILE} s3 sync --acl public-read --cache-control max-age=300 \
   ./build \
-  s3://manage.fusebit.io
+  s3://${S3_BUCKET}
 
 aws --profile=${AWS_PROFILE} s3 cp --acl public-read --cache-control max-age=0 \
   build/index.html	\
-  s3://manage.fusebit.io/index.html
+  s3://${S3_BUCKET}/index.html
 
 aws cloudfront create-invalidation --profile ${AWS_PROFILE} --distribution-id ${CLOUDFRONT_ID} --paths '/*'
