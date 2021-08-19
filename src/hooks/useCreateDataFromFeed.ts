0eb26cfd4457db5c9595a12ed6e7c2fe61a790b6
gfaraone <gfaraone@litebox.ai>
2021-08-18 22:57:47 -0300

    Upgrade navigation to history push

--- src/hooks/useCreateDataFromFeed.ts
+++ src/hooks/useCreateDataFromFeed.ts
@@ -69,6 +69,7 @@ export const useCreateDataFromFeed = () => {
         return false;
       }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [
       createConnector,
       createError,
