f482737c226186402793a4439c81b1818a7d1b4f
MikeFuster <85583835+MikeFuster@users.noreply.github.com>
2021-08-30 18:12:27 -0300

    Mfuster/eng 497 convert portal to entitystate (#66)
    * added useTableLogic hook
    
    * using tableLogic hook on integrations
    
    * using tableLogic hook on connectors
    
    * using tableLogic on users, changed handleRowDelete to work on users aswell
    
    * changed hook name

---
+++ src/hooks/useEntityTable.ts
@@ -0,0 +1,196 @@
+import { useEffect, useRef, useState } from 'react';
+import { useContext } from './useContext';
+import { Integration } from '../interfaces/integration';
+import { Connector } from '../interfaces/connector';
+import { useLoader } from './useLoader';
+import { useAccountIntegrationDeleteIntegration } from './api/v2/account/integration/useDeleteOne';
+import { useAccountConnectorDeleteConnector } from './api/v2/account/connector/useDeleteOne';
+import { useAccountUserDeleteOne } from './api/v1/account/user/useDeleteOne';
+import { Operation } from '../interfaces/operation';
+import { useError } from './useError';
+import { useHistory } from 'react-router';
+import { Feed } from '../interfaces/feed';
+import { Data } from '../interfaces/feedPicker';
+import { useCreateDataFromFeed } from './useCreateDataFromFeed';
+import { useQuery } from './useQuery';
+
+interface Props {
+  headless?: any;
+  setHeadless?: Function;
+  reloadIntegrations?: Function;
+  reloadConnectors?: Function;
+  reloadUsers?: Function;
+  integrations?: {
+    data: {
+      items: Integration[];
+    };
+  };
+  connectors?: {
+    data: {
+      items: Connector[];
+    };
+  };
+}
+
+export const useEntityTable = ({
+  headless,
+  setHeadless,
+  reloadIntegrations,
+  reloadConnectors,
+  reloadUsers,
+  integrations,
+  connectors,
+}: Props) => {
+  const history = useHistory();
+  const { userData } = useContext();
+  const [selected, setSelected] = useState<string[]>([]);
+  const [rows, setRows] = useState<Integration[] | Connector[]>([]);
+  const { waitForOperations, createLoader, removeLoader } = useLoader();
+  const deleteIntegration = useAccountIntegrationDeleteIntegration<Operation>();
+  const deleteConnector = useAccountConnectorDeleteConnector<Operation>();
+  const deleteAccount = useAccountUserDeleteOne<Operation>();
+  const { createError } = useError();
+  const { createDataFromFeed } = useCreateDataFromFeed();
+  const [addIntegrationOpen, setAddIntegrationOpen] = useState(false);
+  const [addConnectorOpen, setAddConnectorOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const query = useQuery();
+  const isIntegration = useRef(false);
+
+  const setItems = () => {
+    const items = isIntegration.current ? integrations?.data.items : connectors?.data.items;
+    items && setRows(items);
+  };
+
+  const checkQuery = () => {
+    setHeadless && setHeadless(false); // so we only do this once.
+    const key = query.get('key');
+    if (key !== null && key !== undefined) {
+      isIntegration.current ? setAddIntegrationOpen(true) : setAddConnectorOpen(true);
+    }
+  };
+
+  useEffect(() => {
+    isIntegration.current = window.location.href.indexOf('integration') >= 0;
+    if (integrations && integrations.data.items) {
+      setLoading(false);
+      // If there are integrations to show or if all of the integrations where deleted we call the setItems function
+      (integrations.data.items.length > 0 || !headless.current) && setItems();
+
+      // If we have just navigated to the integrations list we check if there is a query param
+      headless.current && checkQuery();
+    } else if (connectors && connectors.data.items) {
+      setLoading(false);
+      // If there are connectors to show or if all of the connectors where deleted we call the setItems function
+      (connectors.data.items.length > 0 || !headless.current) && setItems();
+
+      // If we have just navigated to the connectors list we check if there is a query param
+      headless.current && checkQuery();
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [integrations, connectors, query]);
+
+  const handleSelectAllCheck = (event: any) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((row) => row.id);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleCheck = (event: any, id: string) => {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected: string[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+
+    setSelected(newSelected);
+  };
+
+  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+
+  const handleRowDelete = async () => {
+    try {
+      createLoader();
+      let operationIds: string[] = [];
+      for (let i = 0; i < selected.length; i++) {
+        if (isIntegration.current) {
+          const response = await deleteIntegration.mutateAsync({
+            id: selected[i],
+            accountId: userData.accountId,
+            subscriptionId: userData.subscriptionId,
+          });
+          operationIds.push(response.data.operationId);
+        } else if (window.location.href.indexOf('connector') >= 0) {
+          const response = await deleteConnector.mutateAsync({
+            id: selected[i],
+            accountId: userData.accountId,
+            subscriptionId: userData.subscriptionId,
+          });
+          operationIds.push(response.data.operationId);
+        } else {
+          const response = await deleteAccount.mutateAsync({ userId: selected[i], accountId: userData.accountId });
+          operationIds.push(response.data.operationId);
+        }
+      }
+      await waitForOperations(operationIds);
+      isIntegration.current
+        ? reloadIntegrations && reloadIntegrations()
+        : reloadConnectors
+        ? reloadConnectors()
+        : reloadUsers && reloadUsers();
+      setSelected([]);
+    } catch (e) {
+      createError(e.message);
+    } finally {
+      removeLoader();
+    }
+  };
+
+  const handleRowClick = (event: any, href: string) => {
+    if (!event.target.id) {
+      history.push(href);
+    }
+  };
+
+  const handleIntegrationCreation = async (activeIntegration: Feed, data: Data) => {
+    const res = await createDataFromFeed(activeIntegration, data);
+    if (!res) {
+      setAddIntegrationOpen(false);
+    }
+  };
+
+  const handleConnectorCreation = async (activeIntegration: Feed, data: Data) => {
+    const res = await createDataFromFeed(activeIntegration, data, true);
+    if (!res) {
+      setAddConnectorOpen(false);
+    }
+  };
+
+  return {
+    rows,
+    handleSelectAllCheck,
+    handleCheck,
+    isSelected,
+    handleRowDelete,
+    handleRowClick,
+    handleIntegrationCreation,
+    handleConnectorCreation,
+    setAddIntegrationOpen,
+    setAddConnectorOpen,
+    addIntegrationOpen,
+    addConnectorOpen,
+    loading,
+    selected,
+  };
+};
