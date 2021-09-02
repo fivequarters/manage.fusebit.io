a2ca28b23a9935be0e7a3e6f63e587a4ecfdebdc
Martin Fuster <mfuster@litebox.ai>
2021-09-01 23:07:51 -0300

    added table component to connectors

--- src/components/Connectors/Overview/Overview/Overview.tsx
+++ src/components/Connectors/Overview/Overview/Overview.tsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import * as SC from './styles';
-import * as CSC from '../../../globalStyle';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Button,
-  Checkbox,
-  IconButton,
-  Tooltip,
-  Modal,
-  Backdrop,
-  TablePagination,
-} from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
 import { useContext } from '../../../../hooks/useContext';
 import { useAccountConnectorsGetAll } from '../../../../hooks/api/v2/account/connector/useGetAll';
 import { Connector } from '../../../../interfaces/connector';
-import arrowRight from '../../../../assets/arrow-right.svg';
-import arrowLeft from '../../../../assets/arrow-left.svg';
-import { Feed } from '../../../../interfaces/feed';
-import FeedPicker from '../../../FeedPicker';
 import { OverviewProps } from '../../../../interfaces/connectors';
 import { ConnectorCells } from '../../../../interfaces/tableRow';
-import { Data } from '../../../../interfaces/feedPicker';
-import Row from './Row';
-import { useEntityTable } from '../../../../hooks/useEntityTable';
-import { usePagination } from '../../../../hooks/usePagination';
+import TableComponent from '../../../TableComponent';
 
 const Overview: React.FC<OverviewProps> = ({ headless, setHeadless }) => {
   const { userData } = useContext();
@@ -39,20 +14,6 @@ const Overview: React.FC<OverviewProps> = ({ headless, setHeadless }) => {
     subscriptionId: userData.subscriptionId,
   });
   const [selectedCell, setSelectedCell] = React.useState<ConnectorCells>(ConnectorCells.TYPE);
-  const { page, rowsPerPage, handleChangePage, handleChangeRowsPerPage, ROWS_PER_PAGE_OPTIONS } = usePagination();
-  const {
-    handleSelectAllCheck,
-    handleCheck,
-    isSelected,
-    handleRowDelete,
-    handleRowClick,
-    handleConnectorCreation,
-    loading,
-    addConnectorOpen,
-    setAddConnectorOpen,
-    rows,
-    selected,
-  } = useEntityTable({ headless, setHeadless, reloadConnectors, connectors });
 
   const handlePreviousCellSelect = () => {
     if (selectedCell === ConnectorCells.TYPE) {
@@ -71,160 +32,16 @@ const Overview: React.FC<OverviewProps> = ({ headless, setHeadless }) => {
   };
 
   return (
-    <SC.Wrapper>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={addConnectorOpen}
-        onClose={() => setAddConnectorOpen(false)}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-      >
-        <FeedPicker
-          onSubmit={(activeIntegration: Feed, data: Data) => handleConnectorCreation(activeIntegration, data)}
-          open={addConnectorOpen}
-          onClose={() => setAddConnectorOpen(false)}
-        />
-      </Modal>
-      <SC.ButtonContainer>
-        <SC.ButtonMargin>
-          <Button
-            onClick={() => setAddConnectorOpen(true)}
-            startIcon={<AddIcon />}
-            variant="outlined"
-            color="primary"
-            size="large"
-          >
-            New Connector
-          </Button>
-        </SC.ButtonMargin>
-      </SC.ButtonContainer>
-      <SC.DeleteWrapper active={selected.length > 0}>
-        {selected.length > 0 && (
-          <>
-            {selected.length} selected
-            <SC.DeleteIconWrapper>
-              <Tooltip title="Delete">
-                <IconButton onClick={handleRowDelete}>
-                  <DeleteIcon />
-                </IconButton>
-              </Tooltip>
-            </SC.DeleteIconWrapper>
-          </>
-        )}
-      </SC.DeleteWrapper>
-      {!loading && rows.length > 0 ? (
-        <>
-          <SC.Table>
-            <Table size="small" aria-label="Overview Table">
-              <TableHead>
-                <TableRow>
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      color="primary"
-                      checked={rows.length > 0 && selected.length === rows.length}
-                      onChange={handleSelectAllCheck}
-                      inputProps={{ 'aria-label': 'select all connectors' }}
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <SC.Flex>
-                      <SC.ArrowUp />
-                      Name
-                    </SC.Flex>
-                  </TableCell>
-                  <TableCell align="left">Type</TableCell>
-                  <TableCell align="left">Identities</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                  <Row
-                    key={row.id}
-                    row={row}
-                    handleCheck={handleCheck}
-                    handleRowClick={handleRowClick}
-                    isSelected={isSelected}
-                  />
-                ))}
-              </TableBody>
-            </Table>
-          </SC.Table>
-          <SC.TableMobile>
-            <Table size="small" aria-label="Overview Table">
-              <TableHead>
-                <TableRow>
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      color="primary"
-                      checked={rows.length > 0 && selected.length === rows.length}
-                      onChange={handleSelectAllCheck}
-                      inputProps={{ 'aria-label': 'select all connectors' }}
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <SC.Flex>
-                      <SC.ArrowUp />
-                      Name
-                    </SC.Flex>
-                  </TableCell>
-                  <TableCell align="left">
-                    <SC.TableCellMobile>
-                      <p>{selectedCell}</p>
-                      <SC.LeftArrow
-                        onClick={handlePreviousCellSelect}
-                        src={arrowLeft}
-                        alt="previous-cell"
-                        height="16"
-                        width="16"
-                      />
-                      <SC.RightArrow
-                        onClick={handleNextCellSelect}
-                        src={arrowRight}
-                        alt="next-cell"
-                        height="16"
-                        width="16"
-                      />
-                    </SC.TableCellMobile>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                  <Row
-                    key={row.id}
-                    row={row}
-                    handleCheck={handleCheck}
-                    handleRowClick={handleRowClick}
-                    isSelected={isSelected}
-                    mobile={true}
-                    selectedCell={selectedCell}
-                  />
-                ))}
-              </TableBody>
-            </Table>
-          </SC.TableMobile>
-          <TablePagination
-            rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-          />
-        </>
-      ) : (
-        !loading && (
-          <SC.NoConnectorsWrapper>Your connectors list is empty, please create a connector</SC.NoConnectorsWrapper>
-        )
-      )}
-      {loading && (
-        <CSC.LoaderContainer>
-          <CSC.Spinner />
-        </CSC.LoaderContainer>
-      )}
-    </SC.Wrapper>
+    <TableComponent
+      connectorTable={true}
+      headless={headless}
+      setHeadless={setHeadless}
+      connectors={connectors}
+      reloadConnectors={reloadConnectors}
+      selectedCell={selectedCell}
+      handleNextCellSelect={handleNextCellSelect}
+      handlePreviousCellSelect={handlePreviousCellSelect}
+    />
   );
 };
 
