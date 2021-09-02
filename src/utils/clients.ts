e6186af0c8d78301a5e4abe3133a777ea8c920e4
Bruno Krebs <krebs.bruno@gmail.com>
2021-09-02 16:14:37 -0300

    more "avoid error on 404";

--- src/utils/clients.ts
+++ src/utils/clients.ts
@@ -5,17 +5,12 @@ const { REACT_APP_FUSEBIT_DEPLOYMENT } = process.env;
 
 const axiosNo404MiddlewareInstance = axios.create();
 
-export function deleteClient(user: User, clientId: string) {
+export async function removeClient(user: User, clientId: any) {
   const { accountId, token } = user;
   const clientPath = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}/client/${clientId}`;
-  return axiosNo404MiddlewareInstance.delete(clientPath, { headers: { authorization: `bearer ${token}` } });
-}
-
-export function patchClient(user: User, clientId: string, partialClient: any) {
-  const { accountId, token } = user;
-  const clientPath = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}/client/${clientId}`;
-  return axiosNo404MiddlewareInstance.patch(clientPath, partialClient, {
+  return axiosNo404MiddlewareInstance.delete(clientPath, {
     headers: { authorization: `bearer ${token}` },
+    validateStatus: (status) => status === 204 || status === 404,
   });
 }
 
