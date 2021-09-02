ca84630227bd75bb32a2478f268290dfa141c082
Bruno Krebs <krebs.bruno@gmail.com>
2021-09-02 15:43:00 -0300

    refactoring in process;

--- src/components/IntegrationDetail/Develop/Develop/CreateBackendClient.tsx
+++ src/components/IntegrationDetail/Develop/Develop/CreateBackendClient.tsx
@@ -1,12 +1,8 @@
 import { Button } from '@material-ui/core';
 import { useLoader } from '../../../../hooks/useLoader';
 import { useError } from '../../../../hooks/useError';
-import { generateKeyPair } from '../../../../utils/crypto';
-import { generateNonExpiringToken } from '../../../../utils/jwt';
 import { useContext } from '../../../../hooks/useContext';
-import { createBackendClient, getBackendClients } from '../../../../utils/backendClients';
-import { createIssuer } from '../../../../utils/issuer';
-import { addClientIdentity, createClient } from '../../../../utils/clients';
+import { createBackendClient } from '../../../../utils/backendClients';
 
 export default function CreateBackendClient() {
   const { userData } = useContext();
@@ -16,19 +12,8 @@ export default function CreateBackendClient() {
   const registerBackend = async () => {
     try {
       createLoader();
-      const currentBackendList = await getBackendClients(userData);
-      if (currentBackendList.length >= 5) {
-        throw new Error('You have reached the limit of 5 backend clients registered at Fusebit.');
-      }
-      const { data: client } = await createClient(userData);
-      const keyPairToken1 = await generateKeyPair();
-      const { data: issuer } = await createIssuer(userData, client, keyPairToken1);
-      await addClientIdentity(userData, client.id, issuer);
-      await createBackendClient(userData, client, issuer);
-
-      const nonExpiringToken1 = await generateNonExpiringToken(keyPairToken1, issuer, client.id);
-
-      console.log(nonExpiringToken1);
+      const nonExpiringToken = await createBackendClient(userData);
+      console.log(nonExpiringToken);
     } catch (e) {
       createError(e.message);
     } finally {
--- src/utils/backendClients.ts
+++ src/utils/backendClients.ts
@@ -1,12 +1,37 @@
 import axios from 'axios';
 import { User } from '../interfaces/user';
-import { deleteClient } from './clients';
+import { addClientIdentity, createClient, deleteClient } from './clients';
 import { BACKEND_LIST_STORAGE_ID } from './constants';
-import { removeIssuer } from './issuer';
+import { generateKeyPair } from './crypto';
+import { generateNonExpiringToken } from './jwt';
+import { createIssuer, removeIssuer } from './issuer';
 
 const { REACT_APP_FUSEBIT_DEPLOYMENT } = process.env;
 
 const axiosNo404MiddlewareInstance = axios.create();
+
+export async function createBackendClient(user: User) {
+  const currentBackendList = await getBackendClients(user);
+
+  if (currentBackendList.length >= 5) {
+    throw new Error('You have reached the limit of 5 backend clients registered at Fusebit.');
+  }
+
+  const { data: client } = await createClient(user);
+  const keyPairToken1 = await generateKeyPair();
+  const { data: issuer } = await createIssuer(user, client, keyPairToken1);
+  await addClientIdentity(user, client.id, issuer);
+
+  const clientDetails = {
+    id: client.id,
+    issuer: issuer.id,
+  };
+  const currentBackends = await getBackendClients(user);
+  const backends = [...currentBackends, clientDetails];
+
+  await putBackendClients(user, backends);
+  return generateNonExpiringToken(keyPairToken1, issuer, client.id);
+}
 
 export async function removedBackendClient(user: User, clientId: string) {
   const clients = await getBackendClients(user);
@@ -36,18 +61,7 @@ export async function getBackendClients(user: User) {
   }
 }
 
-export async function createBackendClient(user: User, client: any, issuer: any) {
-  const clientDetails = {
-    id: client.id,
-    issuer: issuer.id,
-  };
-  const currentBackends = await getBackendClients(user);
-  const backends = [...currentBackends, clientDetails];
-
-  await putBackendClients(user, backends);
-}
-
-export function putBackendClients(user: User, backendClients: any[]) {
+function putBackendClients(user: User, backendClients: any[]) {
   const { accountId, subscriptionId, token } = user;
   const clientsPaths = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}/subscription/${subscriptionId}/storage/${BACKEND_LIST_STORAGE_ID}`;
   return axiosNo404MiddlewareInstance.put(
