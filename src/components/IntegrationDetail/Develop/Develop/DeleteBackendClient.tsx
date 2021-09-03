88d32333ddca7b0376a76375aa42751e70d30727
Bruno Krebs <brunokrebs@users.noreply.github.com>
2021-09-03 08:28:29 -0300

    ENG-480 Backend Client Management (#67)
    This PR introduces the bare bones needed to manage backend clients that Daisy will register through the portal to get non-expiring API keys.
    
    Here you will find three thin React components:
    
    - CreateBackendClient: to showcase backend client creation
    - DeleteBackendClient: to showcase backend client deletion
    - ListBackendClients: to showcase backend client listing
    
    The components themselves are not that interesting because the business logic is actually created here:
    
    - backendClients.ts: backend client utilities
    - clients.ts:Fusebit client utilities
    - crypto.ts: Web Crypto wrapper to generate PKIs
    - issuer.ts: Fusebit issuer utilities
    - jwt.ts: JWT signing utilities

---
+++ src/components/IntegrationDetail/Develop/Develop/DeleteBackendClient.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { Button } from '@material-ui/core';
+
+import { useLoader } from '../../../../hooks/useLoader';
+import { useError } from '../../../../hooks/useError';
+import { useContext } from '../../../../hooks/useContext';
+import { removedBackendClient } from '../../../../utils/backendClients';
+
+export default function DeleteBackendClient() {
+  const { userData } = useContext();
+  const [backendClientId, setBackendClientID] = useState('');
+  const { createLoader, removeLoader } = useLoader();
+  const { createError } = useError();
+
+  const removeBackendClientListener = async () => {
+    try {
+      createLoader();
+      await removedBackendClient(userData, backendClientId);
+    } catch (e) {
+      createError(e.message);
+    } finally {
+      removeLoader();
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '10px' }}>
+      <input
+        style={{ width: '100%' }}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBackendClientID(e.target.value)}
+        placeholder="Backend ID"
+      />
+      <Button
+        onClick={removeBackendClientListener}
+        style={{ width: '100%' }}
+        size="large"
+        variant="outlined"
+        color="primary"
+      >
+        Remove
+      </Button>
+    </div>
+  );
+}
---
+++ src/utils/clients.ts
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import { Client } from '../interfaces/client';
+import { Issuer } from '../interfaces/issuer';
+import { User } from '../interfaces/user';
+
+const { REACT_APP_FUSEBIT_DEPLOYMENT } = process.env;
+
+const axiosNo404MiddlewareInstance = axios.create();
+
+export async function removeClient(user: User, clientId: string): Promise<void> {
+  const { accountId, token } = user;
+  const clientPath = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}/client/${clientId}`;
+  await axiosNo404MiddlewareInstance.delete<Client>(clientPath, {
+    headers: { authorization: `bearer ${token}` },
+    validateStatus: (status) => status === 204 || status === 404,
+  });
+}
+
+export async function getClient(user: User, clientId: string): Promise<Client> {
+  const { accountId, token } = user;
+  const clientPath = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}/client/${clientId}`;
+  const response = await axiosNo404MiddlewareInstance.get<Client>(clientPath, {
+    headers: { authorization: `bearer ${token}` },
+  });
+  return response.data;
+}
+
+export async function addClientIdentity(user: User, clientId: string, issuer: Issuer): Promise<Client> {
+  const client = await getClient(user, clientId);
+
+  const { accountId, token } = user;
+  const identities = client.identities || [];
+  identities.push({
+    issuerId: issuer.id,
+    subject: client.id,
+  });
+  const partialClient = {
+    identities,
+  };
+  const clientPath = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}/client/${clientId}`;
+  const response = await axiosNo404MiddlewareInstance.patch<Client>(clientPath, partialClient, {
+    headers: { authorization: `bearer ${token}` },
+  });
+  return response.data;
+}
+
+export async function createClient(user: User): Promise<Client> {
+  const { accountId, token } = user;
+  const clientPath = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}/client`;
+  const client = {
+    displayName: 'My Backend',
+    access: {
+      allow: [
+        {
+          action: '*',
+          resource: `/account/${accountId}/`,
+        },
+      ],
+    },
+  };
+  const response = await axiosNo404MiddlewareInstance.post<Client>(clientPath, client, {
+    headers: { authorization: `bearer ${token}` },
+  });
+  return response.data;
+}
+
+export async function patchClient(user: User, client: Partial<Client>): Promise<Client> {
+  const { accountId, token } = user;
+  const clientPath = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}/client/${client.id}`;
+  const response = await axiosNo404MiddlewareInstance.patch<Client>(clientPath, client, {
+    headers: { authorization: `bearer ${token}` },
+  });
+  return response.data;
+}
