6caf65fcf87e3d4006dd3dbdca822cb0a51d9f06
Federico Peralta <fperalta@litebox.ai>
2021-09-13 12:02:47 -0300

    add optimistic result in integration delete

--- src/hooks/api/v2/account/integration/useDeleteOne.ts
+++ src/hooks/api/v2/account/integration/useDeleteOne.ts
@@ -1,9 +1,11 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { Params } from '../../../../../interfaces/api';
 import { useAxios } from '../../../../useAxios';
+import { ACCOUNT_INTEGRATIONS_GET_ALL } from './useGetAll';
 
 export const useAccountIntegrationDeleteIntegration = <T>() => {
   const { axios } = useAxios();
+  const queryClient = useQueryClient()
 
   return useMutation(
     (params: Params) => {
@@ -11,8 +13,32 @@ export const useAccountIntegrationDeleteIntegration = <T>() => {
       return axios<T>(`/v2/account/${accountId}/subscription/${subscriptionId}/integration/${data.id}`, 'delete');
     },
     {
-      onMutate: (_: Params) => () => {},
+      onMutate: async ({ id }: Params) => {
+        await queryClient.cancelQueries(ACCOUNT_INTEGRATIONS_GET_ALL)
+
+        const previous = queryClient.getQueryData<any>(ACCOUNT_INTEGRATIONS_GET_ALL)
+
+        const newItems = [...(previous?.data?.items || [])]
+
+        const index = newItems.findIndex(item => item.id === id)
+
+        newItems.splice(index, 1)
+
+        queryClient.setQueryData(ACCOUNT_INTEGRATIONS_GET_ALL, (old: any) => (({
+          ...old, data: {
+            items: newItems,
+            total: previous?.data?.total - 1
+          }
+        })))
+
+        return () => {
+          queryClient.setQueryData(ACCOUNT_INTEGRATIONS_GET_ALL, previous)
+        }
+      },
       onError: (_, __, rollback) => rollback?.(),
+      onSettled: () => {
+        queryClient.invalidateQueries(ACCOUNT_INTEGRATIONS_GET_ALL)
+      }
     }
   );
 };
--- src/hooks/api/v2/account/integration/useGetAll.ts
+++ src/hooks/api/v2/account/integration/useGetAll.ts
@@ -2,11 +2,13 @@ import { useQuery } from 'react-query';
 import { Params } from '../../../../../interfaces/api';
 import { useAxios } from '../../../../useAxios';
 
+export const ACCOUNT_INTEGRATIONS_GET_ALL = 'accountIntegrationsGetAll'
+
 export const useAccountIntegrationsGetAll = <T>(params: Params) => {
   const { axios } = useAxios();
 
   return useQuery(
-    'accountIntegrationsGetAll',
+    ACCOUNT_INTEGRATIONS_GET_ALL,
     () => axios<T>(`/v2/account/${params.accountId}/subscription/${params.subscriptionId}/integration`, 'get', params),
     { enabled: !!params.enabled }
   );
