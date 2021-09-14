2150d49fa034bbe5faa9e4cd91d448e8db81ef93
Martin Fuster <mfuster@litebox.ai>
2021-09-14 18:01:03 -0300

    added informational banner component

---
+++ src/components/InformationalBanner/InformationalBanner.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as SC from './styles';
+
+interface Props {
+  active: boolean;
+  description: string;
+  highlightedDescription: string;
+  href: string;
+}
+
+const InformationalBanner: React.FC<Props> = ({ active, description, highlightedDescription, href }) => {
+  if (active) {
+    return (
+      <SC.Banner>
+        <SC.InfoIcon />
+        <SC.Description>
+          {description + ' '}
+          <strong>
+            {highlightedDescription.split(' ').map((word: string, index, array) => {
+              if (word === array[array.length - 1]) {
+                return (
+                  <a href={href} target="_blank" rel="noreferrer">
+                    {word}
+                  </a>
+                );
+              }
+              return word + ' ';
+            })}
+          </strong>
+        </SC.Description>
+      </SC.Banner>
+    );
+  }
+
+  return null;
+};
+
+export default InformationalBanner;
