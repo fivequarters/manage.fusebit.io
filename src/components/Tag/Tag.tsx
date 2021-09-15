d769dfa6915fa737d4c8e5d708d3bf474c89c678
MikeFuster <85583835+MikeFuster@users.noreply.github.com>
2021-09-15 17:40:11 -0300

    [ENG-639] Tag component (#100)
    * added tag component
    
    * change onClick

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import * as SC from './styles';
+
+interface Props {
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+const Tag: React.FC<Props> = ({ children, onClick }) => {
+  return (
+    <SC.Wrapper cursorPointer={!!onClick} onClick={() => onClick?.()}>
+      <SC.Tag>{children}</SC.Tag>
+    </SC.Wrapper>
+  );
+};
+
+export default Tag;
