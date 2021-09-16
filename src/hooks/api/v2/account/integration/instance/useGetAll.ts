642aded5c1a2fe034f240e3d2aa0ba85a4dc52a0
Martin Fuster <mfuster@litebox.ai>
2021-09-16 11:45:55 -0300

    add useOptimisticDelete to instance

--- src/hooks/api/v2/account/integration/instance/useGetAll.ts
+++ src/hooks/api/v2/account/integration/instance/useGetAll.ts
@@ -3,6 +3,8 @@ import { Params } from '../../../../../../interfaces/api';
 import { ApiResponse, useAxios } from '../../../../../useAxios';
 import { useContext } from '../../../../../useContext';
 
+export const ACCOUNT_INTEGRATION_INSTANCE_GET_ALL = 'accountIntegrationInstanceGetAll';
+
 export const useAccountIntegrationInstanceGetAll = <T>(
   { id }: Params,
   options?: UseQueryOptions<unknown, unknown, ApiResponse<T>>
@@ -17,7 +19,7 @@ export const useAccountIntegrationInstanceGetAll = <T>(
   };
 
   return useQuery(
-    ['accountIntegrationInstanceGetAll', params],
+    [ACCOUNT_INTEGRATION_INSTANCE_GET_ALL, params],
     () =>
       axios<T>(
         `/v2/account/${params.accountId}/subscription/${params.subscriptionId}/integration/${params.id}/instance`,
