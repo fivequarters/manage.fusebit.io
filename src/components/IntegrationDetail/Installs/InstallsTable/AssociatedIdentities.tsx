4986ea56fe01a3e0b1b264f9bb5fba6ba83ee420
Federico Peralta <fperalta@litebox.ai>
2021-09-21 14:11:12 -0300

    add redirect from associated identities

--- src/components/IntegrationDetail/Installs/InstallsTable/AssociatedIdentities.tsx
+++ src/components/IntegrationDetail/Installs/InstallsTable/AssociatedIdentities.tsx
@@ -1,4 +1,6 @@
+import { useHistory } from 'react-router-dom';
 import { useAccountIntegrationInstanceIdentitiesGetAll } from '../../../../hooks/api/v2/account/integration/instance/identities/useGetAll';
+import { useGetRedirectLink } from '../../../../hooks/useGetRedirectLink';
 import AsyncTags from '../../../AsyncTags';
 
 interface Props {
@@ -7,12 +9,16 @@ interface Props {
 
 const AssociatedIdentities = ({ tenantId }: Props) => {
   const { isLoading, data } = useAccountIntegrationInstanceIdentitiesGetAll({ tenantId }, { enabled: !!tenantId });
+  const history = useHistory();
+  const { getRedirectLink } = useGetRedirectLink();
 
   return (
     <AsyncTags
       isLoading={isLoading}
       tags={(data || [])?.map((i) => ({
         value: i.id,
+        onClick: () =>
+          history.push(getRedirectLink(`/connector/${i.tags['fusebit.parentEntityId']}/identities?expanded=${i.id}`)),
       }))}
     />
   );
