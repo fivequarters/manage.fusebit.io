5524ea6c932f214c43ffb9fc5d0a4119b4d1819b
Federico Peralta <fperalta@litebox.ai>
2021-09-27 18:14:37 -0300

    run prettier

--- src/hooks/api/v2/account/integration/useTestOne.ts
+++ src/hooks/api/v2/account/integration/useTestOne.ts
@@ -7,26 +7,28 @@ export const useAccountIntegrationTestIntegration = () => {
   const { axios } = useAxios();
   const { userData } = useContext();
 
-  return useMutation((params: Params) => {
-    const { id, tenantId, method = 'post', url = `api/tenant/${encodeURIComponent(tenantId)}/test` } = params;
+  return useMutation(
+    (params: Params) => {
+      const { id, tenantId, method = 'post', url = `api/tenant/${encodeURIComponent(tenantId)}/test` } = params;
 
-    return axios(
-      `/v2/account/${userData.accountId}/subscription/${userData.subscriptionId
-      }/integration/${id}/${url}`,
-      method,
-    );
-  }, {
-    onMutate: (params: Params) => {
-      //@ts-ignore
-      window.test.startRun(`${params.method || 'post'}`.toUpperCase())
+      return axios(
+        `/v2/account/${userData.accountId}/subscription/${userData.subscriptionId}/integration/${id}/${url}`,
+        method
+      );
     },
-    onError: (err) => {
-      //@ts-ignore
-      window.test.finishRun(JSON.stringify(err))
-    },
-    onSuccess: (res) => {
-      //@ts-ignore
-      window.test.finishRun(null, { ...(res?.fullResponse || {}), text: JSON.stringify(res.data) })
+    {
+      onMutate: (params: Params) => {
+        //@ts-ignore
+        window.test.startRun(`${params.method || 'post'}`.toUpperCase());
+      },
+      onError: (err) => {
+        //@ts-ignore
+        window.test.finishRun(JSON.stringify(err));
+      },
+      onSuccess: (res) => {
+        //@ts-ignore
+        window.test.finishRun(null, { ...(res?.fullResponse || {}), text: JSON.stringify(res.data) });
+      },
     }
-  })
+  );
 };
