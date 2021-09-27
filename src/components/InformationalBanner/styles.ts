38e9295198247270dc494b89aa7d833f99a4c858
Martin Fuster <mfuster@litebox.ai>
2021-09-27 13:05:33 -0300

    remove padding

--- src/components/InformationalBanner/styles.ts
+++ src/components/InformationalBanner/styles.ts
@@ -4,7 +4,6 @@ import info from '../../assets/info.svg';
 export const Banner = styled.div`
   display: flex;
   align-items: center;
-  padding: 24px;
   border-radius: 8px;
   margin-bottom: 48px;
 `;
