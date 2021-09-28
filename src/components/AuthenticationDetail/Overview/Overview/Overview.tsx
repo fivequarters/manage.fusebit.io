0008981e67585ab5eb494e8bc27c7e8a3eb00898
Martin Fuster <mfuster@litebox.ai>
2021-09-28 10:37:31 -0300

    cli acces cleanup

--- src/components/AuthenticationDetail/Overview/Overview/Overview.tsx
+++ src/components/AuthenticationDetail/Overview/Overview/Overview.tsx
@@ -18,7 +18,6 @@ import { Account } from '../../../../interfaces/account';
 import { useLoader } from '../../../../hooks/useLoader';
 import { useError } from '../../../../hooks/useError';
 import { useCapitalize } from '../../../../hooks/useCapitalize';
-import { useCreateToken } from '../../../../hooks/useCreateToken';
 import { useAccountUserDeleteOne } from '../../../../hooks/api/v1/account/user/useDeleteOne';
 import { useGetRedirectLink } from '../../../../hooks/useGetRedirectLink';
 import { useCopy } from '../../../../hooks/useCopy';
@@ -91,11 +90,9 @@ const Overview: React.FC = () => {
   const [cliOpen, setCliOpen] = React.useState(false);
   const [deleteOpen, setDeleteOpen] = React.useState(false);
   const [popperOpen, setPopperOpen] = React.useState(false);
-  const [token, setToken] = React.useState('');
   const { createLoader, removeLoader } = useLoader();
   const { createError } = useError();
   const { capitalize } = useCapitalize();
-  const { _createToken } = useCreateToken();
   const deleteAccount = useAccountUserDeleteOne<Operation>();
   const { getRedirectLink } = useGetRedirectLink();
   const { handleCopy, copiedLine } = useCopy();
@@ -140,23 +137,6 @@ const Overview: React.FC = () => {
     setData(accountData?.data);
   };
 
-  const handleCliOpen = async () => {
-    if (token === '') {
-      try {
-        createLoader();
-        const token = await _createToken(accountData?.data.id || '');
-        setToken(token);
-        setCliOpen(true);
-      } catch (e) {
-        createError(e);
-      } finally {
-        removeLoader();
-      }
-    } else {
-      setCliOpen(true);
-    }
-  };
-
   const handleDelete = async () => {
     try {
       createLoader();
@@ -195,7 +175,7 @@ const Overview: React.FC = () => {
         closeAfterTransition
         BackdropComponent={Backdrop}
       >
-        <CliAccess token={token} open={cliOpen} onClose={() => setCliOpen(false)} />
+        <CliAccess open={cliOpen} onClose={() => setCliOpen(false)} />
       </Modal>
       {accountData?.data.id === userId ? (
         <SC.UserCard>
@@ -299,7 +279,7 @@ const Overview: React.FC = () => {
       <SC.CLIAccesWrapper>
         <SC.CLIAccess>Command Line (CLI) Access</SC.CLIAccess>
         <Button
-          onClick={handleCliOpen}
+          onClick={() => setCliOpen(true)}
           style={{ width: '200px' }}
           fullWidth={false}
           size="large"
