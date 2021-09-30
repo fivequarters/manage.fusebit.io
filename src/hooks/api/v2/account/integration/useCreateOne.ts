4c193d7f6cc084e8c68ac7c510124b889215fcde
Martin Fuster <mfuster@litebox.ai>
2021-09-30 14:49:43 -0300

    create one

--- src/hooks/api/v2/account/integration/useCreateOne.ts
+++ src/hooks/api/v2/account/integration/useCreateOne.ts
@@ -13,10 +13,9 @@ export const useAccountIntegrationCreateIntegration = <T>() => {
       return axios<T>(`/v2/account/${accountId}/subscription/${subscriptionId}/integration/${data.id}`, 'post', data);
     },
     {
-      onMutate: (_: Params) => () => { },
+      onMutate: (_: Params) => () => {},
       onError: (_, __, rollback) => rollback?.(),
       onSuccess: (_, { id }) => {
-
         localStorage.setItem(
           id,
           JSON.stringify({
@@ -28,7 +27,7 @@ export const useAccountIntegrationCreateIntegration = <T>() => {
           })
         );
 
-        queryClient.removeQueries('accountIntegrationsGetAll')
+        queryClient.removeQueries('accountIntegrationsGetAll');
       },
     }
   );
