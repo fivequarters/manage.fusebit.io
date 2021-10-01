83198ccba5ece41c6a3fe8383599ac06fe2da440
Martin Fuster <mfuster@litebox.ai>
2021-10-01 16:47:17 -0300

    extend textareaAtusizeProps

--- src/components/FormFields/Textarea/Textarea.tsx
+++ src/components/FormFields/Textarea/Textarea.tsx
@@ -2,12 +2,10 @@ import { TextareaAutosizeProps } from '@material-ui/core';
 import React from 'react';
 import * as SC from './styles';
 
-interface FieldVariant {
+interface Props extends TextareaAutosizeProps {
   fieldVariant: 'default' | 'customBlue';
   hasError?: boolean;
 }
-
-type Props = TextareaAutosizeProps & FieldVariant;
 
 const Textarea: React.FC<Props> = ({ fieldVariant, hasError, ...props }) => {
   return <SC.MUITextarea fieldVariant={fieldVariant} hasError={hasError} {...props} />;
