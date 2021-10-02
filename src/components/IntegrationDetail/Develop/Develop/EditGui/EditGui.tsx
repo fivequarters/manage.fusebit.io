ec85902629270880b12ec05defd4be1e99e8d36c
Benn Bollay <bb.github@magitech.org>
2021-10-01 23:12:37 -0700

    745. Change 'Edit Locally' to point to the docs

--- src/components/IntegrationDetail/Develop/Develop/EditGui/EditGui.tsx
+++ src/components/IntegrationDetail/Develop/Develop/EditGui/EditGui.tsx
@@ -137,7 +137,9 @@ const EditGui = React.forwardRef(({ onClose, onMount, integrationId }: Props, re
             </ButtonGroup>
             <h3>{integrationId}</h3>
             <SC.ActionsHelpWrapper>
-              <SC.ActionsHelpLink href="/">Edit Locally</SC.ActionsHelpLink>
+              <SC.ActionsHelpLink href="https://developer.fusebit.io/docs/developing-locally">
+                Edit locally
+              </SC.ActionsHelpLink>
               <SC.ActionsHelpImage src={question} alt="question" height="16" width="16" />
             </SC.ActionsHelpWrapper>
             <SC.Close onClick={handleClose} />
