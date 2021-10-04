7822cf6777a5527503c5254a112772b480a95175
Bruno Krebs <brunokrebs@users.noreply.github.com>
2021-10-04 17:20:13 -0300

    ENG-748 Update Segment events on management portal (#160)
    Update some Segment events on management portal and memoize trackEvent

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,6 +1,28 @@
-export function trackEvent(eventName: string, objectLocation: string) {
+import isEqual from 'lodash.isequal';
+
+type TrackEventHandler = (eventName: string, objectLocation: string, extraProperties?: { [key: string]: any }) => void;
+
+// utility to memoize the original trackEvent function to avoid noise
+const memoize = (originalFunction: TrackEventHandler) => {
+  let lastParamsUsed: any[];
+  const memoizedFunction: TrackEventHandler = (...args) => {
+    if (isEqual(lastParamsUsed, args)) {
+      return;
+    }
+    lastParamsUsed = args;
+    originalFunction(...args);
+  };
+  return memoizedFunction;
+};
+
+// original trackEvent function that gets called to offload events to Segment
+const trackEventHandler: TrackEventHandler = (eventName, objectLocation, extraProperties = {}) => {
   analytics.track(eventName, {
     objectLocation,
     domain: 'manage.fusebit.io',
+    ...extraProperties,
   });
-}
+};
+
+// trackEvent is memoized because React re-rendering process makes it get called multiple times for the same event
+export const trackEvent = memoize(trackEventHandler);
