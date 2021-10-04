9d036096f5fc3a51ab3500701080903c4d5a49e9
Federico Peralta <fperalta@litebox.ai>
2021-10-04 18:46:17 -0300

    fix syntax

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,10 +1,6 @@
 import { useCallback, useState } from "react"
 
-type UseModalData = [
-    open: boolean,
-    setOpen: (open: boolean) => void,
-    toogle: () => void,
-]
+type UseModalData = [open: boolean, setOpen: (open: boolean) => void, toogle: () => void]
 
 export const useModal = (defaultOpen?: boolean): UseModalData => {
     const [open, setOpen] = useState(!!defaultOpen)
