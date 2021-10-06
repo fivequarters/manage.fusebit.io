a22626821612cde7c8a4742790e990cbbf0ebddb
MikeFuster <85583835+MikeFuster@users.noreply.github.com>
2021-10-06 18:10:40 -0300

    [ENG-743] editor should show full integration url (#176)
    * [Hot-Fix] Add mobile editor (#172)
    
    * check editor exists before use it
    
    * fix run called multiple times
    
    * run prettier
    
    * test multiple calls
    
    * add semi
    
    * console key changed
    
    * clean code
    
    * add missing console.log
    
    * run prettier
    
    * add enableListener
    
    * run prettier
    
    * show full integration url
    
    Co-authored-by: Federico Peralta <88199805+fperaltalitebox@users.noreply.github.com>
    Co-authored-by: gfaraone <48419008+gfaraone@users.noreply.github.com>

--- src/hooks/api/v2/account/integration/useTestOne.ts
+++ src/hooks/api/v2/account/integration/useTestOne.ts
@@ -3,10 +3,16 @@ import { Params } from '../../../../../interfaces/api';
 import { useAxios } from '../../../../useAxios';
 import { useContext } from '../../../../useContext';
 import { getIntegrationConfig } from '../../../../../utils/localStorage';
+import { useGetRedirectLink } from '../../../../useGetRedirectLink';
+import { useParams } from 'react-router-dom';
 
 export const useAccountIntegrationTestIntegration = () => {
+  const { id: integrationId } = useParams<{ id: string }>();
   const { axios } = useAxios({ ignoreInterceptors: true });
   const { userData } = useContext();
+  const { REACT_APP_FUSEBIT_DEPLOYMENT } = process.env;
+  const { getRedirectLink } = useGetRedirectLink();
+  const integrationBaseUrl = `${REACT_APP_FUSEBIT_DEPLOYMENT}/v2${getRedirectLink(`/integration/${integrationId}`)}`;
 
   return useMutation(
     (params: Params) => {
@@ -31,7 +37,10 @@ export const useAccountIntegrationTestIntegration = () => {
         const { method, url } = getIntegrationConfig(params.id).runner;
 
         window.editor?.serverLogsEntry(
-          JSON.stringify({ msg: `Sending ${method.toUpperCase()} request to ${url}`, level: 30 })
+          JSON.stringify({
+            msg: `Sending ${method.toUpperCase()} request to ${integrationBaseUrl}${url}`,
+            level: 30,
+          })
         );
       },
       onError: (err) => {
