fca123d10188ba6d03e03a2bd142d3b94b5abdd8
Federico Peralta <fperalta@litebox.ai>
2021-10-07 14:56:15 -0300

    remove encapsulation

--- src/pages/IntegrationDetailDevelopPage.tsx
+++ src/pages/IntegrationDetailDevelopPage.tsx
@@ -1,15 +1,18 @@
-import React, { FC, ReactElement } from 'react';
+import { FC, ReactElement } from 'react';
 import { useParams } from 'react-router-dom';
 import Layout from '../components/Layout';
 import { useAccountIntegrationsGetOne } from '../hooks/api/v2/account/integration/useGetOne';
 import { useContext } from '../hooks/useContext';
 import { Integration } from '../interfaces/integration';
-import IntegrationDetailDevelop from '../components/IntegrationDetail';
 import Navbar from '../components/common/Navbar';
 import { useTrackPage } from '../hooks/useTrackPage';
+import TabComponent from '../components/common/TabComponent';
+import Develop from '../components/IntegrationDetail/Develop';
+import { useGetRedirectLink } from '../hooks/useGetRedirectLink';
 
 const IntegrationDetailDevelopPage: FC<{}> = (): ReactElement => {
   const { id } = useParams<{ id: string }>();
+  const { getRedirectLink } = useGetRedirectLink();
   const { userData } = useContext();
   const { data: integrationData } = useAccountIntegrationsGetOne<Integration>({
     enabled: userData.token,
@@ -23,7 +26,10 @@ const IntegrationDetailDevelopPage: FC<{}> = (): ReactElement => {
   return (
     <Layout>
       <Navbar sectionName={integrationData?.data.id || id} dropdown integrationsLink />
-      <IntegrationDetailDevelop id={id} />
+      <TabComponent
+        tabNames={['Develop', 'Installs']}
+        tabObjects={[<Develop key="develop" />, getRedirectLink(`/integration/${id}/installs`)]}
+      />
     </Layout>
   );
 };
