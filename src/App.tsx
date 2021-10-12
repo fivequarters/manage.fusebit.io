e7459eaa47131d70f51f3673b1a1815c10042544
Federico Peralta <fperalta@litebox.ai>
2021-10-12 13:24:13 -0300

    move query provider to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { useEffect } from 'react';
 import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
@@ -13,8 +12,6 @@ import { ContextProvider } from './hooks/useContext';
 import { validateToken } from './utils/utils';
 
 function App() {
-  const queryClient = new QueryClient();
-
   useEffect(() => {
     validateToken();
   }, []);
@@ -26,36 +23,34 @@ function App() {
           <title>{APP_TITLE}</title>
         </Helmet>
         <ContextProvider>
-          <QueryClientProvider client={queryClient}>
-            {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
-            <ThemeProvider theme={responsiveFontSizes(createMuiTheme(lightTheme))}>
-              <CookieConsent
-                location="bottom"
-                buttonText="Accept"
-                cookieName="CookieConsent"
-                style={{ background: 'rgba(215, 229, 255, 0.4)' }}
-                buttonStyle={{
-                  borderRadius: '4px',
-                  border: '1px solid #F83420',
-                  backgroundColor: 'white',
-                  color: '#F83420',
-                  fontSize: '13px',
-                }}
-                expires={150}
-              >
-                <p style={{ color: '#333333', fontWeight: 500 }}>
-                  This website uses cookies to enhance the user experience.
-                </p>
-              </CookieConsent>
-              <Router>
-                <Switch>
-                  {routes.map((route: RouteItem) => (
-                    <Route key={`${route.key}`} path={`${route.path}`} component={route.component} exact />
-                  ))}
-                </Switch>
-              </Router>
-            </ThemeProvider>
-          </QueryClientProvider>
+          {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+          <ThemeProvider theme={responsiveFontSizes(createMuiTheme(lightTheme))}>
+            <CookieConsent
+              location="bottom"
+              buttonText="Accept"
+              cookieName="CookieConsent"
+              style={{ background: 'rgba(215, 229, 255, 0.4)' }}
+              buttonStyle={{
+                borderRadius: '4px',
+                border: '1px solid #F83420',
+                backgroundColor: 'white',
+                color: '#F83420',
+                fontSize: '13px',
+              }}
+              expires={150}
+            >
+              <p style={{ color: '#333333', fontWeight: 500 }}>
+                This website uses cookies to enhance the user experience.
+              </p>
+            </CookieConsent>
+            <Router>
+              <Switch>
+                {routes.map((route: RouteItem) => (
+                  <Route key={`${route.key}`} path={`${route.path}`} component={route.component} exact />
+                ))}
+              </Switch>
+            </Router>
+          </ThemeProvider>
         </ContextProvider>
       </HelmetProvider>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import Session from './Session';
 
 const session = new URLSearchParams(window.location.search).get('session');
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>{session ? <Session session={session} /> : <App />}</React.StrictMode>,
+  <React.StrictMode>
+    {session ? (
+      <Session session={session} />
+    ) : (
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    )}
+  </React.StrictMode>,
   document.getElementById('root')
 );
