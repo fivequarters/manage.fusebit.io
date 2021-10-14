f79beb3c74bb2b7ed172e8805bac0f1de28f4726
Jacob Haller-Roby <jacob@fusebit.io>
2021-10-13 19:00:03 -0700

    Smaller button label

--- src/components/IntegrationDetail/Develop/Develop/Connect/LinkSampleApp.tsx
+++ src/components/IntegrationDetail/Develop/Develop/Connect/LinkSampleApp.tsx
@@ -19,17 +19,6 @@ export const LinkSampleApp: React.FC<IProps> = ({ componentMap }: IProps) => {
     getSampleAppUrl();
   }, [user, componentMap]);
 
-  const types = Object.keys(componentMap);
-  if (types.length > 1) {
-    types[types.length - 1] = `and ${types[types.length - 1]}`;
-  }
-  let typesString: string;
-  if (types.length > 2) {
-    typesString = Object.keys(componentMap).join(', ');
-  } else {
-    typesString = Object.keys(componentMap).join(' ');
-  }
-
   return url ? (
     <Button
       style={{ margin: '0 auto', width: '293px' }}
@@ -40,7 +29,7 @@ export const LinkSampleApp: React.FC<IProps> = ({ componentMap }: IProps) => {
       color="primary"
       size="large"
     >
-      Try the sample app for {typesString}!
+      Run a Sample App!
     </Button>
   ) : (
     <></>
