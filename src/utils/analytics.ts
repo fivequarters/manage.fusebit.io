9c587496ee2dc1b8e556f225926bf35dd15cb03d
Federico Peralta <fperalta@litebox.ai>
2021-10-19 16:32:42 -0300

    fix naming

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -52,10 +52,10 @@ const trackEventHandler: TrackEventHandler = (eventName, objectLocation, extraPr
 // trackEvent is memoized because React re-rendering process makes it get called multiple times for the same event
 export const trackEvent = memoize(trackEventHandler);
 
-export const trackAuthEvent = (user: Auth0Token) => {
+export const trackAuthEvent = (auth0DecodedToken: Auth0Token) => {
   const isSilentAuthInProgress = silentAuthInProgress();
-  const fusebitProfile = user['https://fusebit.io/profile'];
-  const userSignUpEvent = user['https://fusebit.io/new-user'] === true;
+  const fusebitProfile = auth0DecodedToken['https://fusebit.io/profile'];
+  const userSignUpEvent = auth0DecodedToken['https://fusebit.io/new-user'] === true;
   const currentSegmentUserId = analytics.user().id();
   const sameUser = fusebitProfile.userId === currentSegmentUserId;
 
@@ -77,7 +77,7 @@ export const trackAuthEvent = (user: Auth0Token) => {
   }
 
   analytics.identify(fusebitProfile.userId, {
-    ...user,
+    ...auth0DecodedToken,
   } as Object);
   trackEvent('Log In Execution', 'Authentication', extraSegmentEventProps);
 };
