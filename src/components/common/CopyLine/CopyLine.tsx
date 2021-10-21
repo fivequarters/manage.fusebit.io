c449fa832ff463659569ca70d782a3652ee2cfdf
Federico Peralta <88199805+fperaltalitebox@users.noreply.github.com>
2021-10-21 14:09:45 -0300

    ENG 872 Remove weird noise (#235)
    * remove weird noise
    
    * remove px from zero

--- src/components/common/CopyLine/CopyLine.tsx
+++ src/components/common/CopyLine/CopyLine.tsx
@@ -4,6 +4,8 @@ import copyIcon from '../../../assets/copy.svg';
 import { Props } from '../../../interfaces/copyLine';
 import { useCopy } from '../../../hooks/useCopy';
 
+// TODO: Refactor this component and remove extra logic as highlightedText. Use Typography From MUI instead of plain text.
+
 const CopyLine: React.FC<Props> = ({ text, highlightedText, horizontalScrollbar, warning, onCopy, disableCopy }) => {
   const [fadeChange, setFadeChange] = React.useState(false);
   const { handleCopy, copiedLine } = useCopy();
@@ -29,21 +31,23 @@ const CopyLine: React.FC<Props> = ({ text, highlightedText, horizontalScrollbar,
       </SC.LineInstructionCopy>
       <SC.LineInstructionFade disabled={disableCopy} warning={warning} change={fadeChange} />
       <SC.LineInstruction warning={warning} horizontalScrollbar={horizontalScrollbar}>
-        {highlightedText && <span className="unselectable">$</span>}
-        {highlightedText
-          ? text.split(' ').map((word) => {
-              let foundWord = false;
-              highlightedText.split(' ').forEach((wordToHightlight) => {
-                if (wordToHightlight.match(word)) {
-                  foundWord = true;
+        <p>
+          {highlightedText && <span className="unselectable">$</span>}
+          {highlightedText
+            ? text.split(' ').map((word) => {
+                let foundWord = false;
+                highlightedText.split(' ').forEach((wordToHightlight) => {
+                  if (wordToHightlight.match(word)) {
+                    foundWord = true;
+                  }
+                });
+                if (foundWord) {
+                  return <strong key={word}>{word}</strong>;
                 }
-              });
-              if (foundWord) {
-                return <strong key={word}>{word}</strong>;
-              }
-              return `${word} `;
-            })
-          : text}
+                return `${word} `;
+              })
+            : text}
+        </p>
       </SC.LineInstruction>
       <SC.CopySuccess copy={copiedLine}>Copied to clipboard!</SC.CopySuccess>
     </SC.LineInstructionWrapper>
