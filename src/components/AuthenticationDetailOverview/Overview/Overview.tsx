f3838abc2fb70a69b88a0a4a61d30b969aa21361
Martin Fuster <mfuster@litebox.ai>
2021-10-27 17:53:11 -0300

    change authentication detail links

--- src/components/AuthenticationDetailOverview/Overview/Overview.tsx
+++ src/components/AuthenticationDetailOverview/Overview/Overview.tsx
@@ -3,24 +3,24 @@ import { Button, Modal, Backdrop } from '@material-ui/core';
 import { ValidationMode } from '@jsonforms/core';
 
 import { useHistory, useParams } from 'react-router-dom';
-import dots from '../../../assets/dots.svg';
-import { useAuthContext } from '../../../hooks/useAuthContext';
-import * as CSC from '../../globalStyle';
+import dots from '@components/assets/dots.svg';
+import { useAuthContext } from '@hooks/useAuthContext';
+import CliAccess from '@components/AuthenticationDetailOverview/CliAccess';
+import { useAccountUserGetOne } from '@hooks/api/v1/account/user/useGetOne';
+import { useAccountUserUpdateOne } from '@hooks/api/v1/account/user/useUpdateOne';
+import accountImg from '@components/assets/account.svg';
+import { Operation } from '@interfaces/operation';
+import { Account } from '@interfaces/account';
+import { useLoader } from '@hooks/useLoader';
+import { useError } from '@hooks/useError';
+import { useAccountUserDeleteOne } from '@hooks/api/v1/account/user/useDeleteOne';
+import { useGetRedirectLink } from '@hooks/useGetRedirectLink';
+import { useCopy } from '@hooks/useCopy';
+import { startCase } from '@utils/utils';
+import ConfirmationPrompt from '@components/common/ConfirmationPrompt';
+import BaseJsonForm from '@components/common/BaseJsonForm';
+import * as CSC from '@components/globalStyle';
 import * as SC from './styles';
-import CliAccess from '../CliAccess';
-import { useAccountUserGetOne } from '../../../hooks/api/v1/account/user/useGetOne';
-import { useAccountUserUpdateOne } from '../../../hooks/api/v1/account/user/useUpdateOne';
-import accountImg from '../../../assets/account.svg';
-import { Operation } from '../../../interfaces/operation';
-import { Account } from '../../../interfaces/account';
-import { useLoader } from '../../../hooks/useLoader';
-import { useError } from '../../../hooks/useError';
-import { useAccountUserDeleteOne } from '../../../hooks/api/v1/account/user/useDeleteOne';
-import { useGetRedirectLink } from '../../../hooks/useGetRedirectLink';
-import { useCopy } from '../../../hooks/useCopy';
-import { startCase } from '../../../utils/utils';
-import ConfirmationPrompt from '../../common/ConfirmationPrompt';
-import BaseJsonForm from '../../common/BaseJsonForm';
 
 const schema = {
   type: 'object',
