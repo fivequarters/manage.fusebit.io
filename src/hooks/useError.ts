e98c335820b97c5bedda0c317207394253e1f03e
Federico Peralta <peralta.federico.manuel@gmail.com>
2021-10-31 11:50:12 -0300

    add space

--- src/hooks/useError.ts
+++ src/hooks/useError.ts
@@ -6,6 +6,7 @@ export const useError = () => {
   const createError = (err: any) => {
     // the element 0 shows the key, the 1 show the value, i only want the value so i get the element 1
     const message = err?.response?.data?.message?.split(':')?.[1] || 'There was an error';
+
     enqueueSnackbar(message, {
       variant: 'error',
       preventDuplicate: true,
