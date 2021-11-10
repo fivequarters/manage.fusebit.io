6b5cd27a84830e7ae789dc3bd161031328ac8e82
Martin Fuster <mfuster@litebox.ai>
2021-11-10 15:00:14 -0300

    remove unused dependancies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
-import { HelmetProvider } from 'react-helmet-async';
 import CookieConsent from 'react-cookie-consent';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import DashboardRoutes from '@components/DashboardRoutes';
@@ -7,30 +6,28 @@ import { ContextProvider } from '@hooks/useAuthContext';
 import { lightTheme } from './theme/appTheme';
 
 const App = () => (
-  <HelmetProvider>
-    <ContextProvider>
-      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
-      <ThemeProvider theme={responsiveFontSizes(createMuiTheme(lightTheme))}>
-        <CookieConsent
-          location="bottom"
-          buttonText="Accept"
-          cookieName="CookieConsent"
-          style={{ background: 'rgba(215, 229, 255, 0.4)' }}
-          buttonStyle={{
-            borderRadius: '4px',
-            border: '1px solid #F83420',
-            backgroundColor: 'white',
-            color: '#F83420',
-            fontSize: '13px',
-          }}
-          expires={150}
-        >
-          <p style={{ color: '#333333', fontWeight: 500 }}>This website uses cookies to enhance the user experience.</p>
-        </CookieConsent>
-        <DashboardRoutes />
-      </ThemeProvider>
-    </ContextProvider>
-  </HelmetProvider>
+  <ContextProvider>
+    {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+    <ThemeProvider theme={responsiveFontSizes(createMuiTheme(lightTheme))}>
+      <CookieConsent
+        location="bottom"
+        buttonText="Accept"
+        cookieName="CookieConsent"
+        style={{ background: 'rgba(215, 229, 255, 0.4)' }}
+        buttonStyle={{
+          borderRadius: '4px',
+          border: '1px solid #F83420',
+          backgroundColor: 'white',
+          color: '#F83420',
+          fontSize: '13px',
+        }}
+        expires={150}
+      >
+        <p style={{ color: '#333333', fontWeight: 500 }}>This website uses cookies to enhance the user experience.</p>
+      </CookieConsent>
+      <DashboardRoutes />
+    </ThemeProvider>
+  </ContextProvider>
 );
 
 export default App;
