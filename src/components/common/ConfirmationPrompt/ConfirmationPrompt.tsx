33ab9bc7bd0c7b8efff1dc6b26e1dbfc9e19f04c
Martin Fuster <mfuster@litebox.ai>
2021-11-15 17:40:28 -0300

    refactor confirmation prompt

--- src/components/common/ConfirmationPrompt/ConfirmationPrompt.tsx
+++ src/components/common/ConfirmationPrompt/ConfirmationPrompt.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Button, Modal, Backdrop } from '@material-ui/core';
 import * as CSC from '@components/globalStyle';
-import * as SC from './styles';
+
+const StyledCard = styled.div<{ open: boolean }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  opacity: ${(props) => (props.open ? 1 : 0)};
+  padding: 64px;
+  width: 480px;
+  border-radius: 8px;
+  box-shadow: 0px 20px 48px rgba(52, 72, 123, 0.1);
+  outline: transparent;
+  transition: all 1s linear;
+
+  @media only screen and (max-width: 550px) {
+    width: 100%;
+    left: 0;
+    top: auto;
+    bottom: 0;
+    transform: translate(0, 0);
+  }
+`;
+
+const StyledButtonsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+`;
 
 interface Props {
   open: boolean;
@@ -36,11 +66,11 @@ const ConfirmationPrompt: React.FC<Props> = ({
       closeAfterTransition
       BackdropComponent={Backdrop}
     >
-      <SC.Card open={open} tabIndex={-1}>
+      <StyledCard open={open} tabIndex={-1}>
         <CSC.Close onClick={() => setOpen(false)} />
         <CSC.ModalTitle margin="0 0 16px 0">{title}</CSC.ModalTitle>
         <CSC.ModalDescription textAlign="center">{description}</CSC.ModalDescription>
-        <SC.ButtonsWrapper>
+        <StyledButtonsWrapper>
           {!hideCancelButton && (
             <Button
               onClick={() => setOpen(false)}
@@ -55,8 +85,8 @@ const ConfirmationPrompt: React.FC<Props> = ({
           <Button onClick={onConfirm} style={{ width: '77px' }} size="medium" variant="contained" color="primary">
             {confirmationButtonText || 'Delete'}
           </Button>
-        </SC.ButtonsWrapper>
-      </SC.Card>
+        </StyledButtonsWrapper>
+      </StyledCard>
     </Modal>
   );
 };
