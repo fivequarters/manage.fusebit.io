429a351385e45916744b2b9eb10829d56f26665f
Martin Fuster <mfuster@litebox.ai>
2021-11-16 11:23:31 -0300

    add integrationsFeed hook

---
+++ src/hooks/useGetIntegrationsFeed.ts
@@ -0,0 +1,30 @@
+import http from 'http';
+import { Params } from '@interfaces/api';
+import { Feed } from '@interfaces/feed';
+import { useQuery } from 'react-query';
+
+const { REACT_APP_INTEGRATIONS_FEED_URL } = process.env;
+
+export const integrationsFeed = async (): Promise<Feed[]> => {
+  return new Promise((accept) => {
+    const req = http.get(
+      REACT_APP_INTEGRATIONS_FEED_URL || 'http://localhost:3000/feed/integrationsFeed.json',
+      (res) => {
+        let data = '';
+        res.on('data', (stream) => {
+          data += stream;
+        });
+        res.on('end', () => accept(JSON.parse(data)));
+      }
+    );
+    req.on('error', () => {
+      accept([]);
+    });
+  });
+};
+
+export const useAccountGetOne = (params: Params) => {
+  return useQuery(['accountGetOne'], integrationsFeed, {
+    enabled: !!params.enabled,
+  });
+};
