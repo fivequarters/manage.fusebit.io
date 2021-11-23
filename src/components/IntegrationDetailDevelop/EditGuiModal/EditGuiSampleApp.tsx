44193e4697c990703946acbee72a642c3452add9
Federico Peralta <fperalta@litebox.ai>
2021-11-23 16:23:08 -0300

    fix styles

--- src/components/IntegrationDetailDevelop/EditGuiModal/EditGuiSampleApp.tsx
+++ src/components/IntegrationDetailDevelop/EditGuiModal/EditGuiSampleApp.tsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthContext } from '@hooks/useAuthContext';
 import { createSampleAppClientUrl } from '@utils/backendClients';
+import styled from 'styled-components';
 
 interface IProps {
   componentMap: Record<string, string>;
 }
 
+const StyledSampleApp = styled.a`
+  font-size: 14px;
+  line-height: 20px;
+  color: var(--black);
+  text-decoration: underline;
+  margin-right: 10px;
+`;
+
+// TODO: Remove duplication with LinkSampleApp
 export const EditGuiSampleApp: React.FC<IProps> = ({ componentMap }) => {
   const { userData: user } = useAuthContext();
   const [url, setUrl] = useState<string>();
@@ -17,9 +27,9 @@ export const EditGuiSampleApp: React.FC<IProps> = ({ componentMap }) => {
   }, [user, componentMap]);
 
   return url ? (
-    <a target="_blank" rel="noreferrer" href={url}>
+    <StyledSampleApp target="_blank" rel="noreferrer" href={url}>
       Run a Sample App
-    </a>
+    </StyledSampleApp>
   ) : (
     <></>
   );
