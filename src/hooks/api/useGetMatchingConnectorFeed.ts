a9f244977542a4f97a7b8075ea84d41f9650aa1f
Federico Peralta <fperalta@litebox.ai>
2021-11-26 12:07:08 -0300

    remove integrations feed fallback

--- src/hooks/api/useGetMatchingConnectorFeed.ts
+++ src/hooks/api/useGetMatchingConnectorFeed.ts
@@ -1,28 +1,17 @@
 import { useQuery } from 'react-query';
 import { Connector } from '@interfaces/connector';
 import { FinalConnector } from '@interfaces/integrationDetailDevelop';
-import { connectorsFeed as getConnectorsFeed, integrationsFeed as getIntegrationsFeed } from '../../static/feed';
+import { connectorsFeed as getConnectorsFeed } from '../../static/feed';
 
 export const findMatchingConnectorFeed = async (connector: Connector | FinalConnector) => {
-  let connectorData;
   const handler = connector.data?.handler;
   const connectorsFeed = await getConnectorsFeed();
 
-  connectorData = connectorsFeed.find((item) => {
-    const entities = Object.keys(item.configuration.entities);
-
-    return entities.find((entity) => item.configuration.entities[entity].data.handler === handler);
-  });
-
-  if (!connectorData) {
-    const feedId = connector?.tags?.['fusebit.feedId'];
-
-    const integrationsFeed = await getIntegrationsFeed();
-
-    connectorData = integrationsFeed.find((item) => item.id === feedId);
-  }
-
-  return connectorData;
+  return connectorsFeed.find((item) =>
+    Object.keys(item.configuration.entities).find(
+      (entity) => item.configuration.entities[entity].data.handler === handler
+    )
+  );
 };
 
 interface Props {
