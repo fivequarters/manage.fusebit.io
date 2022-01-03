4699d79f02715a046a14aad61d290225e7446d31
Martin Fuster <mfuster@litebox.ai>
2022-01-03 15:35:08 -0300

    fix modal

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -6,16 +6,13 @@ import * as CSC from '@components/globalStyle';
 
 const StyledTitle = styled(DialogTitle)`
   padding-top: 64px;
-
-  > h2 {
-    font-size: 24px;
-    line-height: 30px;
-    font-weight: 600;
-    max-width: 350px;
-    margin: 0 auto;
-    color: var(--black);
-    text-align: center;
-  }
+  font-size: 24px;
+  line-height: 30px;
+  font-weight: 600;
+  max-width: 350px;
+  margin: 0 auto;
+  color: var(--black);
+  text-align: center;
 `;
 
 export const StyledContent = styled(DialogContent)<{ hasPadding?: boolean }>`
