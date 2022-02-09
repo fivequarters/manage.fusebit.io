794cdb75e81a5e2a07f0022b73e4e5a568bcb6d4
Martin Fuster <mfuster@litebox.ai>
2022-02-09 17:11:31 -0300

    fix editor events on mobile

--- src/components/IntegrationDetailDevelop/FusebitEditor/useEditorEvents.ts
+++ src/components/IntegrationDetailDevelop/FusebitEditor/useEditorEvents.ts
@@ -23,24 +23,26 @@ const useEditorEvents = ({ isMounted, events }: Props) => {
       [EditorEvents.BuildFinished]: () => {
         setIsSaving(false);
       },
-      [EditorEvents.BuildError]: () => (e: { error: { message: string } }) => {
+      [EditorEvents.BuildError]: (e: { error: { message: string } }) => {
         setIsSaving(false);
         setErrorBuild(`There was an error in the build: ${e.error.message}`);
       },
       [EditorEvents.LogsAttached]: () => {
         setLogs((oldLogs) => [...oldLogs, logWithTime('Attached to real-time logs...')]);
       },
-      [EditorEvents.LogsEntry]: () => (e: LogEntry) => {
+      [EditorEvents.LogsEntry]: (e: LogEntry) => {
         const logData = JSON.parse(e.data) as LogData;
         setLogs((oldLogs) => [...oldLogs, logWithTime(logData.msg)]);
       },
-      [EditorEvents.RunnerFinished]: () => (e: LogEntryError) => {
+      [EditorEvents.RunnerFinished]: (e: LogEntryError) => {
         setLogs((oldLogs) => [...oldLogs, logWithTime(e.error)]);
       },
     };
 
     if (isMounted) {
-      events.forEach((e) => window.editor?.on(e, config[e as keyof typeof config]));
+      events.forEach((e) => {
+        window.editor?.on(e, config[e as keyof typeof config]);
+      });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMounted]);
