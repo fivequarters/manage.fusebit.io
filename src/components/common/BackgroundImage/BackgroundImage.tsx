693c625c6c0c73f40e657d13a3723f4071e1e9a4
Martin Fuster <mfuster@litebox.ai>
2022-02-16 12:17:13 -0300

    reduce blur

--- src/components/common/BackgroundImage/BackgroundImage.tsx
+++ src/components/common/BackgroundImage/BackgroundImage.tsx
@@ -16,7 +16,7 @@ const StyledBackgroundImage = styled.div<{ image: string; css?: FlattenSimpleInt
   left: 0px;
   top: -30px;
   opacity: 0.55;
-  filter: blur(1.5px);
+  filter: blur(1px);
   ${(props) => props.css && props.css};
 `;
 
