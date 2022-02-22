e8bef98951814d5e94c25ac308bf6f471a73fcfb
Martin Fuster <mfuster@litebox.ai>
2022-02-22 13:44:20 -0300

    add fullscreen support

--- src/components/IntegrationDetailDevelop/MobileDrawer/MobileDrawer.tsx
+++ src/components/IntegrationDetailDevelop/MobileDrawer/MobileDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { Drawer, Button, Box, Typography } from '@material-ui/core';
 import * as CSC from '@components/globalStyle';
@@ -8,6 +8,12 @@ import CloseIcon from '@material-ui/icons/Close';
 import DeleteIcon from '@material-ui/icons/Delete';
 import useEditorEvents from '../FusebitEditor/useEditorEvents';
 import { EditorEvents } from '~/enums/editor';
+
+const StyledDrawer = styled(Drawer)`
+  .MuiDrawer-paperAnchorBottom {
+    height: 100%;
+  }
+`;
 
 const StyledGuiMobileWrapper = styled.div`
   position: relative;
@@ -39,6 +45,7 @@ const StyledGuiMobileNotSupportedText = styled.div`
 `;
 
 const StyledLogWrapper = styled(Box)`
+  position: relative;
   background-color: var(--secondary-color);
   color: var(--black);
 `;
@@ -50,6 +57,7 @@ const StyledLogTitle = styled(Typography)`
 `;
 
 const StyledLog = styled(Box)`
+  position: relative;
   background-color: #ffffff;
   word-wrap: break-word;
 `;
@@ -67,6 +75,7 @@ const MobileDrawer = ({ open, onClose, handleRun, isRunning, processing }: Props
     isMounted: open,
     events: [EditorEvents.LogsEntry, EditorEvents.LogsAttached, EditorEvents.RunnerFinished],
   });
+  const [logWrapperHeight, setLogWrapperHeight] = useState(0);
 
   useEffect(() => {
     if (logs.length > 0) {
@@ -76,6 +85,40 @@ const MobileDrawer = ({ open, onClose, handleRun, isRunning, processing }: Props
       }
     }
   }, [logs]);
+
+  useEffect(() => {
+    const calculateLogWrapperHeight = (elementHeight: number, viewportHeight: number) => {
+      const guiModalWrapperHeightPorcentage = (elementHeight / viewportHeight) * 100;
+      const logWrapperHeightPorcentage = 100 - guiModalWrapperHeightPorcentage;
+      const LogWrapperHeight = (logWrapperHeightPorcentage * viewportHeight) / 100;
+      return LogWrapperHeight;
+    };
+
+    const setWrapperHeight = (onSetWrapperHeight?: () => void) => {
+      const guiModalWrapper = document.getElementById('guiModalWrapper');
+      const viewportHeight = window.innerHeight;
+      const guiModalWrapperHeight = guiModalWrapper?.offsetHeight;
+
+      if (guiModalWrapperHeight) {
+        setLogWrapperHeight(calculateLogWrapperHeight(guiModalWrapperHeight, viewportHeight));
+        onSetWrapperHeight?.();
+      }
+    };
+
+    const handleResize = () => {
+      setWrapperHeight();
+    };
+
+    const onInit = setInterval(() => {
+      setWrapperHeight(() => {
+        clearInterval(onInit);
+      });
+    }, 50);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [open]);
 
   const handleClose = () => {
     clearLogs();
@@ -96,55 +139,53 @@ const MobileDrawer = ({ open, onClose, handleRun, isRunning, processing }: Props
 
   return (
     <>
-      <Drawer anchor="bottom" open={open} onClose={handleClose}>
-        <>
-          <StyledGuiMobileWrapper>
-            <CSC.CloseWrapper onClick={handleClose}>
-              <CloseIcon fontSize="small" />
-            </CSC.CloseWrapper>
-            <Button
-              startIcon={<img src={play} alt="play" height="16" width="16" />}
-              style={{ width: '200px' }}
-              size="large"
-              variant="contained"
-              color="primary"
-              onClick={handleRun}
-              disabled={isRunning || processing}
-            >
-              {buttonText}
-            </Button>
-            <StyledGuiMobileNotSupportedWrapper>
-              <StyledGuiMobileNotSupportedIcon src={info} alt="not supported" height="16" width="16" />
-              <StyledGuiMobileNotSupportedText>
-                Editing is currently only available on desktop mode
-              </StyledGuiMobileNotSupportedText>
-            </StyledGuiMobileNotSupportedWrapper>
-          </StyledGuiMobileWrapper>
-          <StyledLogWrapper padding="21px 30px 0">
-            <Box display="flex" alignItems="center">
-              <StyledLogTitle>Log Console</StyledLogTitle>
-              <Box marginLeft="auto" color="inherit" onClick={clearLogs}>
-                <DeleteIcon color="inherit" style={{ width: '20px' }} />
-              </Box>
+      <StyledDrawer anchor="bottom" open={open} onClose={handleClose} style={{ position: 'relative', height: '100vh' }}>
+        <StyledGuiMobileWrapper id="guiModalWrapper">
+          <CSC.CloseWrapper onClick={handleClose}>
+            <CloseIcon fontSize="small" />
+          </CSC.CloseWrapper>
+          <Button
+            startIcon={<img src={play} alt="play" height="16" width="16" />}
+            style={{ width: '200px' }}
+            size="large"
+            variant="contained"
+            color="primary"
+            onClick={handleRun}
+            disabled={isRunning || processing}
+          >
+            {buttonText}
+          </Button>
+          <StyledGuiMobileNotSupportedWrapper>
+            <StyledGuiMobileNotSupportedIcon src={info} alt="not supported" height="16" width="16" />
+            <StyledGuiMobileNotSupportedText>
+              Editing is currently only available on desktop mode
+            </StyledGuiMobileNotSupportedText>
+          </StyledGuiMobileNotSupportedWrapper>
+        </StyledGuiMobileWrapper>
+        <StyledLogWrapper height={logWrapperHeight} padding="21px 30px 30px" position="relative">
+          <Box display="flex" alignItems="center">
+            <StyledLogTitle>Log Console</StyledLogTitle>
+            <Box marginLeft="auto" color="inherit" onClick={clearLogs}>
+              <DeleteIcon color="inherit" style={{ width: '20px' }} />
             </Box>
-            <StyledLog
-              id="mobile-log"
-              fontSize="10px"
-              lineHeight="11.5px"
-              fontFamily="Courier"
-              height="165px"
-              padding="15px 7px"
-              borderRadius="4px"
-              marginTop="10px"
-              overflow="scroll"
-            >
-              {logs.map((log) => (
-                <p key={log.id}>{log.msg}</p>
-              ))}
-            </StyledLog>
-          </StyledLogWrapper>
-        </>
-      </Drawer>
+          </Box>
+          <StyledLog
+            id="mobile-log"
+            fontSize="10px"
+            height={logWrapperHeight - 90}
+            lineHeight="11.5px"
+            fontFamily="Courier"
+            padding="15px 7px"
+            borderRadius="4px"
+            marginTop="10px"
+            overflow="scroll"
+          >
+            {logs.map((log) => (
+              <p key={log.id}>{log.msg}</p>
+            ))}
+          </StyledLog>
+        </StyledLogWrapper>
+      </StyledDrawer>
     </>
   );
 };
