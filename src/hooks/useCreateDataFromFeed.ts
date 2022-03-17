b56dc00519969951d6289e27c81c90db1abd1ee5
Federico Peralta <88199805+fperaltalitebox@users.noreply.github.com>
2022-03-17 11:13:20 -0300

    Update src/hooks/useCreateDataFromFeed.ts

--- src/hooks/useCreateDataFromFeed.ts
+++ src/hooks/useCreateDataFromFeed.ts
@@ -63,7 +63,9 @@ export const useCreateDataFromFeed = () => {
 
       return res.find((r) => r.data.entityType === 'integration')?.data;
     } catch (e) {
-      trackEvent('Integration Creation Failed', 'Integrations');
+      trackEvent('Integration Creation Failed', 'Integrations', {
+       {integration: commonTags['fusebit.feedId'],}
+      });
 
       createError(e);
     }
