dda6a6d401ceab58538351da5aadc713de346e32
Martin Fuster <mfuster@litebox.ai>
2022-04-07 07:38:51 -0300

    start building custom video controls

--- src/components/common/Video/Video.tsx
+++ src/components/common/Video/Video.tsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
+import play from '@assets/video-play.svg';
+import volume from '@assets/video-volume.svg';
+import fullscreen from '@assets/video-fullscreen.svg';
+import { Box, Slider } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+
+const StyledWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
 
 const StyledVideo = styled.video`
   position: relative;
   width: 100%;
-  height: 524px;
   height: 100%;
   object-fit: cover;
   border-radius: 8px;
+`;
+
+const StyledControlsWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  border-radius: 8px;
+  padding: 12px;
+  background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(0, 0, 0, 1));
+`;
+
+const StyledIcon = styled.img`
+  height: 24px;
+  width: 24px;
+  object-fit: contain;
+`;
+
+const StyledVolumeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 12px;
+`;
+
+const StyledSlider = styled(Slider)`
+  width: 70px;
+  color: white;
+  margin-left: 14px;
 `;
 
 interface Props extends React.VideoHTMLAttributes<HTMLVideoElement> {
@@ -16,14 +54,56 @@ interface Props extends React.VideoHTMLAttributes<HTMLVideoElement> {
 }
 
 const Video: React.FC<Props> = ({ src, tracks, children, ...props }) => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [volumeLevel, setVolume] = React.useState<number | number[]>(60);
+
+  const handleVolumeChange = (event: React.ChangeEvent<{}>, newVolume: number | number[]) => {
+    setVolume(newVolume);
+    if (videoRef.current) {
+      videoRef.current.volume = (volumeLevel as number) / 100;
+    }
+  };
+
+  const handlePlayState = () => {
+    if (videoRef.current) {
+      const isStopped = videoRef.current.paused;
+      if (isStopped) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  };
+
   return (
-    <StyledVideo src={src} controls {...props}>
-      {tracks.map((track) => (
-        <track key={track.src} {...track} />
-      ))}
-      {children}
-      <p>Your browser does not support HTML5 video.</p>
-    </StyledVideo>
+    <StyledWrapper>
+      <StyledVideo ref={videoRef} src={src} {...props}>
+        {tracks.map((track) => (
+          <track key={track.src} {...track} />
+        ))}
+        {children}
+        <p>Your browser does not support HTML5 video.</p>
+      </StyledVideo>
+      <StyledControlsWrapper>
+        <Box display="flex" alignItems="center">
+          <IconButton color="secondary" onClick={handlePlayState}>
+            <StyledIcon src={play} alt="play" />
+          </IconButton>
+          <StyledVolumeWrapper>
+            <StyledIcon src={volume} alt="volume" />
+            <StyledSlider
+              color="secondary"
+              value={volumeLevel}
+              onChange={handleVolumeChange}
+              aria-labelledby="volume"
+            />
+          </StyledVolumeWrapper>
+          <IconButton color="secondary" style={{ margin: '0 0 0 auto' }}>
+            <StyledIcon src={fullscreen} alt="fullscreen" />
+          </IconButton>
+        </Box>
+      </StyledControlsWrapper>
+    </StyledWrapper>
   );
 };
 
