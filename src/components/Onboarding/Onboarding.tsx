deccf04743b4b71f063e06ea42f2f37eb7b8d703
Martin Fuster <mfuster@litebox.ai>
2022-04-13 12:51:00 -0300

    add cta

--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -1,6 +1,6 @@
 import Modal from '@components/common/Modal';
 import video from '@assets/placeholder-video.mp4';
-import { Backdrop, useMediaQuery } from '@material-ui/core';
+import { Backdrop, Box, Button, useMediaQuery } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useQuery } from '@hooks/useQuery';
@@ -35,14 +35,43 @@ const StyledDescription = styled.p`
   }
 `;
 
+const StyledCtaWrapper = styled(Box)<{ active: boolean }>`
+  visibility: ${(props) => (props.active ? 'visible' : 'hidden')};
+  opacity: ${(props) => (props.active ? 1 : 0)};
+  transition: all 0.25s linear;
+
+  @media only screen and (max-width: 500px) {
+    padding-bottom: 30px;
+  }
+`;
+
+const StyledCtaText = styled.h3`
+  font-size: 20px;
+  line-height: 26px;
+  font-weight: 600;
+  color: var(--black);
+  margin: 0;
+  margin-right: 10px;
+
+  @media only screen and (max-width: 700px) {
+    font-size: 18px;
+    line-height: 20px;
+  }
+
+  @media only screen and (max-width: 500px) {
+    font-size: 16px;
+    line-height: 18px;
+    max-width: calc(100% - 140px);
+  }
+`;
+
 const StyledVideoWrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 450px;
+  height: 420px;
 
   @media only screen and (max-width: 500px) {
     height: 100%;
-    padding-bottom: 20px;
   }
 `;
 
@@ -59,11 +88,12 @@ const Onboarding: React.FC = () => {
 
   useEffect(() => {
     const utmContent = query.get('utm_content');
-    if (utmContent === UTM_CONTENT.NEW_VID && !videoCompleted) {
+    const hasOnboardingBeenCompleted = localStorage.getItem(UTM_CONTENT.NEW_VID);
+    if (utmContent === UTM_CONTENT.NEW_VID && !hasOnboardingBeenCompleted) {
       setOpen(true);
       trackEventMemoized('Product Video Modal Viewed', objectLocation);
     }
-  }, [query, videoCompleted]);
+  }, [query]);
 
   useEffect(() => {
     const handleBeforeUnload = (e: BeforeUnloadEvent) => {
@@ -85,6 +115,7 @@ const Onboarding: React.FC = () => {
   const onEnded = () => {
     trackEventMemoized('Product Video Completed', objectLocation);
     setVideoCompleted(true);
+    localStorage.setItem(UTM_CONTENT.NEW_VID, 'completed');
   };
 
   return (
@@ -111,6 +142,22 @@ const Onboarding: React.FC = () => {
             tracks={[{ src: 'captions_en.vtt', kind: 'captions', srcLang: 'en', label: 'english_captions' }]}
           />
         </StyledVideoWrapper>
+        <StyledCtaWrapper active={videoCompleted} display="flex" alignItems="center" mt="24px">
+          <StyledCtaText>Let’s get going on building your first integration</StyledCtaText>
+          <Button
+            onClick={() => setOpen(false)}
+            size={isMobile ? 'medium' : 'large'}
+            color="primary"
+            variant="contained"
+            style={{
+              width: isMobile ? 'fit-content' : '200px',
+              height: isMobile ? '32px' : '48px',
+              marginLeft: 'auto',
+            }}
+          >
+            Get Started
+          </Button>
+        </StyledCtaWrapper>
       </StyledWrapper>
     </Modal>
   );
