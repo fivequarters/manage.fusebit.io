a3335425a8c21bd701762e8ba68d36a1f474b2bc
Martin Fuster <mfuster@litebox.ai>
2022-04-25 12:39:00 -0300

    integrate logs

--- src/components/IntegrationDetailLogging/Logging/Logging.tsx
+++ src/components/IntegrationDetailLogging/Logging/Logging.tsx
@@ -1,39 +1,23 @@
-import BackgroundImage from '@components/common/BackgroundImage';
-import Card from '@components/common/Card';
-import { StyledListItem } from '@components/globalStyle';
-import { Box, useMediaQuery } from '@material-ui/core';
-import { trackEventMemoized } from '@utils/analytics';
-import { sendIntercomMessage } from '@utils/intercom';
-import { useParams } from 'react-router-dom';
-import logging from '@assets/logging.png';
+import { Box } from '@material-ui/core';
+import styled from 'styled-components';
+
+const StyledLogs = styled.iframe`
+  > #document > html > body .panel-container {
+    background: red;
+  }
+`;
 
 const Logging = () => {
-  const { id } = useParams<{ id: string }>();
-  const isMobile = useMediaQuery('(max-width: 880px)');
-
-  const handleIntercomPost = () => {
-    trackEventMemoized('Talk to Sales Button Clicked', 'Logging (Beta)', { integration: id });
-    window.Intercom?.('showNewMessage', 'I would like to enable the "Logging" feature for my account.');
-    sendIntercomMessage();
-  };
-
   return (
     <Box display="flex" alignItems="center" justifyContent="center" pb="140px" position="relative">
-      {!isMobile && <BackgroundImage image={logging} />}
-      <Card
-        title="See it all **in one place**"
-        description="Spend less time searching and more time resolving incidents with Fusebit's end to end logging and tracing infrastructure."
-        buttonText="Talk to sales"
-        handleClick={handleIntercomPost}
-      >
-        <ul>
-          <StyledListItem>
-            Trace requests end-to-end from your system all the way to third-party APIs and webhooks
-          </StyledListItem>
-          <StyledListItem>Centralized logs for all executions of your integration</StyledListItem>
-          <StyledListItem>Quickly search, filter, and analyze your logs for debugging</StyledListItem>
-        </ul>
-      </Card>
+      <StyledLogs
+        title="iframe"
+        id="iframe"
+        src="https://api.us-west-1.on.fusebit.io/v2/grafana/bootstrap/d-solo/logging/basic?panelId=2&kiosk=&refresh=1s&fusebitAuthorization=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UQkZRemxDTURWR1EwSTBNRGc1UmpOQ1JqSkNRVGczTmpZNFJqRXlNRVUyTTBSRE9ETTROdyJ9.eyJodHRwczovL2Z1c2ViaXQuaW8vcHJvZmlsZSI6eyJhY2NvdW50SWQiOiJhY2MtMzExMzA2NmIzNzFjNDhiNCIsInN1YnNjcmlwdGlvbklkIjoic3ViLWUwZDk4Y2I4NWFkNjRmMjQiLCJ1c2VySWQiOiJ1c3ItMDQ3NjlmYjdjOTA0NGYzNSJ9LCJpc3MiOiJodHRwczovL2F1dGguZnVzZWJpdC5pby8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwMzExMTA3ODgxMjU3NzUxNTkyMSIsImF1ZCI6WyJodHRwczovL2FwaS51cy13ZXN0LTEub24uZnVzZWJpdC5pbyIsImh0dHBzOi8vZnVzZWJpdC5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjUwNjQ1MTk0LCJleHAiOjE2NTA3MzE1OTQsImF6cCI6Ik5JZnFFNGhwUE9YdUlobGxreG5kbGFmU0tjS2VzRWZjIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCJ9.PBj99LPKhDdHmzvSATzT-wXMwTlO8IM_nzfb87idX2-cdo7WnmJr7gFSNA1m7V5NxxO7dHhDW3p1lWq0Ft6BnQFD6bJMgzMuytQX8cFQC_7fQb_LxTQ2xkNVs85PjzChZ-mJqT2gVeCkuzhA9MoH5FHAmf7_238KES_QVvcWjMysQzbv5Vvm099GF9zgb8lYaGRIo8pxIzTGD9pWfXY8WFF5096RnlbZiXiN-1Xb19SgCqCdXNgPbiO-oqDsamJzGYlhtaA3D-auu2kTwfeAdc9IADpMQwznP4RfKdsK4d5StsK5vVM5p7nQtRXHSxlCN7sK3Wfx3zm3tt-XZPbPUg&fusebitAccountId=acc-3113066b371c48b4&var-accountId=acc-3113066b371c48b4&var-subscriptionId=sub-e0d98cb85ad64f24&var-boundaryId=integration&var-functionId=slack"
+        width="450"
+        height="200"
+        frameBorder="0"
+      />
     </Box>
   );
 };
