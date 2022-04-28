f53527ba44c643beb0bcb71b2cc69f7937ba435c
Martin Fuster <mfuster@litebox.ai>
2022-04-28 12:55:48 -0300

    use base url

--- src/components/IntegrationDetailLogging/Logging/Logging.tsx
+++ src/components/IntegrationDetailLogging/Logging/Logging.tsx
@@ -1,13 +1,6 @@
 import { useAuthContext } from '@hooks/useAuthContext';
 import { Box } from '@material-ui/core';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
-
-const StyledLogs = styled.iframe`
-  > #document > html > body .panel-container {
-    background: red;
-  }
-`;
 
 const Logging = () => {
   const { userData } = useAuthContext();
@@ -15,10 +8,10 @@ const Logging = () => {
 
   return (
     <Box display="flex" alignItems="center" justifyContent="center" pb="140px" position="relative">
-      <StyledLogs
+      <iframe
         title="iframe"
         id="iframe"
-        src={`https://api.us-west-1.on.fusebit.io/v2/grafana/bootstrap/d-solo/logging/basic?panelId=2&kiosk=&refresh=1s&fusebitAuthorization=${userData.token}&fusebitAccountId=${userData.accountId}&var-accountId=${userData.accountId}&var-subscriptionId=${userData.subscriptionId}&var-boundaryId=integration&var-functionId=${id}`}
+        src={`${process.env.REACT_APP_FUSEBIT_DEPLOYMENT}/v2/grafana/bootstrap/d-solo/logging/basic?panelId=2&kiosk=&refresh=1s&fusebitAuthorization=${userData.token}&fusebitAccountId=${userData.accountId}&var-accountId=${userData.accountId}&var-subscriptionId=${userData.subscriptionId}&var-boundaryId=integration&var-functionId=${id}`}
         width="100%"
         height="350"
         frameBorder="0"
