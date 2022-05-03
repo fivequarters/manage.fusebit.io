64a35106b325124a7d2e6f7d49fa8435d049f978
Martin Fuster <mfuster@litebox.ai>
2022-05-03 16:16:20 -0300

    style beta tag

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -4,7 +4,6 @@
 import { Tab } from '@interfaces/TabComponent';
 import { Box } from '@material-ui/core';
 import styled from 'styled-components';
-import icon from '@assets/add.svg';
 
 export const BACKEND_LIST_STORAGE_ID = 'fusebit/api-token-clients';
 
@@ -16,21 +15,25 @@ export const PRODUCTION_HOST = 'manage.fusebit.io';
 
 export const INTEGRATION_PROCESSING_SUFFIX = '-pending-processing';
 
-const StyledBeta = styled.img`
-  position: absolute;
-  right: -20px;
-  top: -3px;
-  height: 15px;
-  width: 15px;
-  object-fit: contain;
+const StyledBeta = styled.div`
+  font-size: 10px;
+  line-height: 14px;
+  font-weight: 500;
+  color: var(--black);
+  text-align: center;
+  padding: 2px 6px;
+  background-color: var(--yellow);
+  box-shadow: 0px 8px 8px rgba(52, 72, 123, 0.1);
+  border-radius: 4px;
+  margin-left: 6px;
 `;
 
 export const INTEGRATION_DETAIL_TABNAMES: Tab[] = [
   'Develop',
   'Installs',
-  <Box key="logging" position="relative">
+  <Box key="logging" display="flex" alignItems="center" position="relative">
     Logging
-    <StyledBeta src={icon} alt="beta" height="15" width="15" />
+    <StyledBeta>Beta</StyledBeta>
   </Box>,
   'Health',
   'Reliability',
