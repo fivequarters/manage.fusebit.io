e9c9cb1dabe9435eae2a6465ca847ebee71b5b18
Martin Fuster <mfuster@litebox.ai>
2022-05-05 18:26:44 -0300

    use logs & add link to full grafana

--- src/components/IntegrationDetailLogging/Logging/Logging.tsx
+++ src/components/IntegrationDetailLogging/Logging/Logging.tsx
@@ -1,5 +1,5 @@
 import { useAuthContext } from '@hooks/useAuthContext';
-import { Box } from '@material-ui/core';
+import { Box, Button } from '@material-ui/core';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -10,15 +10,40 @@ const StyledLogs = styled.iframe`
   box-shadow: 0px 1px 30px -1px rgb(52 72 123 / 10%);
 `;
 
+const WEEK_IN_MS = 7 * 24 * 60 * 60 * 1000;
+
+const FROM = Date.now() - WEEK_IN_MS;
+
 const Logging = () => {
   const { userData } = useAuthContext();
   const { id } = useParams<{ id: string }>();
 
+  const handleExplore = () => {
+    window.open(`${process.env.REACT_APP_FUSEBIT_DEPLOYMENT}/v2/grafana?theme=light`, '_blank');
+  };
+
   return (
-    <Box display="flex" alignItems="center" justifyContent="center" height="450px" pb="40px" position="relative">
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="450px"
+      pb="40px"
+      position="relative"
+    >
+      <Button
+        onClick={handleExplore}
+        variant="outlined"
+        color="primary"
+        size="large"
+        style={{ marginLeft: 'auto', marginBottom: '24px' }}
+      >
+        Explore
+      </Button>
       <StyledLogs
         title="logging"
-        src={`${process.env.REACT_APP_FUSEBIT_DEPLOYMENT}/v2/grafana/explore/?kiosk=tv&theme=light&refresh=1s&fusebitAuthorization=${userData.token}&fusebitAccountId=${userData.accountId}&var-accountId=${userData.accountId}&var-subscriptionId=${userData.subscriptionId}&var-boundaryId=integration&var-functionId=${id}`}
+        src={`${process.env.REACT_APP_FUSEBIT_DEPLOYMENT}/v2/grafana/bootstrap/d-solo/logging/basic?panelId=2?kiosk=tv&theme=light&refresh=1s&fusebitAuthorization=${userData.token}&fusebitAccountId=${userData.accountId}&var-accountId=${userData.accountId}&var-subscriptionId=${userData.subscriptionId}&var-boundaryId=integration&var-functionId=${id}&from=${FROM}}`}
         width="100%"
         height="100%"
         frameBorder="0"
