2852355371e9ef664d4f2ec1195deb700df19c94
Martin Fuster <mfuster@litebox.ai>
2022-05-11 14:36:59 -0300

    handle auth callback errors

--- src/pages/AuthCallbackPage.tsx
+++ src/pages/AuthCallbackPage.tsx
@@ -1,5 +1,4 @@
 import { FC, ReactElement, useEffect } from 'react';
-
 import { useHistory, useLocation } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import { getAnalyticsClient, trackAuthEvent } from '@utils/analytics';
@@ -22,18 +21,14 @@ const {
 const getAuth0ProfileAndCompany = async (auth0Token: string, accountId: string) => {
   let auth0Profile = {} as Auth0Profile;
   let company = {} as Company;
-  try {
-    const skipXUserAgent = true;
-    const auth0AxiosClient = createAxiosClient(auth0Token, skipXUserAgent);
-    const { data: userInfo } = await auth0AxiosClient.get(`${REACT_APP_AUTH0_DOMAIN}/userinfo`);
-    auth0Profile = userInfo;
-    const fusebitAxiosClient = createAxiosClient(auth0Token);
-    const { data: account } = await fusebitAxiosClient.get(`${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}`);
-    company = account;
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-  }
+  const skipXUserAgent = true;
+  const auth0AxiosClient = createAxiosClient(auth0Token, skipXUserAgent);
+  const { data: userInfo } = await auth0AxiosClient.get(`${REACT_APP_AUTH0_DOMAIN}/userinfo`);
+  auth0Profile = userInfo;
+  const fusebitAxiosClient = createAxiosClient(auth0Token);
+  const { data: account } = await fusebitAxiosClient.get(`${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}`);
+  company = account;
+
   return {
     auth0Profile,
     company,
@@ -56,6 +51,10 @@ const AuthCallbackPage: FC<{}> = (): ReactElement => {
   const { setAuthStatus, setUserData } = useAuthContext();
   const { setFirstTimeVisitor } = useFirstTimeVisitor();
 
+  const handleError = (error: any) => {
+    history.push(`/logged-out?error=${error}`);
+  };
+
   useEffect(() => {
     try {
       const urlParams = new URLSearchParams(location.hash.substring(1));
@@ -64,8 +63,8 @@ const AuthCallbackPage: FC<{}> = (): ReactElement => {
 
       if (error || !token) {
         if (error && error !== 'login_required') {
-          // eslint-disable-next-line no-console
-          console.error('Callback URL called without token or with an unknown error.', location);
+          const err = `Callback URL called without token or with an unknown error ${location}`;
+          handleError(err);
         }
         signIn();
         return;
@@ -81,51 +80,58 @@ const AuthCallbackPage: FC<{}> = (): ReactElement => {
         // redeem the init token, then redirect back to the home page to re-authenticate and
         // get a new access token with an updated Fusebit profile.
         window.localStorage.removeItem('fusebitInitToken');
-        redeemInitToken(token, fusebitProfile, initToken).then(() => {
-          window.location.href = '/';
-        });
+        redeemInitToken(token, fusebitProfile, initToken)
+          .then(() => {
+            history.push('/');
+          })
+          .catch((err) => {
+            handleError(err);
+          });
         return;
       }
 
-      getAuth0ProfileAndCompany(token, fusebitProfile?.accountId || '').then(({ auth0Profile, company }) => {
-        const normalizedData = {
-          firstName: auth0Profile?.given_name || '',
-          lastName: auth0Profile?.family_name || '',
-          company: company?.displayName || '',
-        };
+      getAuth0ProfileAndCompany(token, fusebitProfile?.accountId || '')
+        .then(({ auth0Profile, company }) => {
+          const normalizedData = {
+            firstName: auth0Profile?.given_name || '',
+            lastName: auth0Profile?.family_name || '',
+            company: company?.displayName || '',
+          };
 
-        // for local testing: https://docs.google.com/document/d/1dkI4UdRgaD840HWc-sGi6_qz4JY8AHts97MD-1O4SpY/edit#heading=h.gtoda0wgke4n
-        if (REACT_APP_FUSEBIT_ACCOUNT_ID && REACT_APP_FUSEBIT_SUBSCRIPTION_ID && REACT_APP_FUSEBIT_USER_ID) {
-          fusebitProfile.accountId = REACT_APP_FUSEBIT_ACCOUNT_ID;
-          fusebitProfile.subscriptionId = REACT_APP_FUSEBIT_SUBSCRIPTION_ID;
-          fusebitProfile.userId = REACT_APP_FUSEBIT_USER_ID;
-        }
+          // for local testing: https://docs.google.com/document/d/1dkI4UdRgaD840HWc-sGi6_qz4JY8AHts97MD-1O4SpY/edit#heading=h.gtoda0wgke4n
+          if (REACT_APP_FUSEBIT_ACCOUNT_ID && REACT_APP_FUSEBIT_SUBSCRIPTION_ID && REACT_APP_FUSEBIT_USER_ID) {
+            fusebitProfile.accountId = REACT_APP_FUSEBIT_ACCOUNT_ID;
+            fusebitProfile.subscriptionId = REACT_APP_FUSEBIT_SUBSCRIPTION_ID;
+            fusebitProfile.userId = REACT_APP_FUSEBIT_USER_ID;
+          }
 
-        setUserData({ token, ...fusebitProfile, ...auth0Profile, ...company, ...normalizedData });
-        setAuthStatus(AuthStatus.AUTHENTICATED);
+          setUserData({ token, ...fusebitProfile, ...auth0Profile, ...company, ...normalizedData });
+          setAuthStatus(AuthStatus.AUTHENTICATED);
 
-        const navigatePostAuth = () => {
-          const urlSearchParams = new URLSearchParams(window.location.search);
-          const requestedPath = (urlSearchParams.get('requestedPath') || '/').replace(/ /g, '+');
-          const requestedSearch = localStorage.getItem('requestedSearch') || '';
-          const requestedHash = localStorage.getItem('requestedHash') || '';
+          const navigatePostAuth = () => {
+            const urlSearchParams = new URLSearchParams(window.location.search);
+            const requestedPath = (urlSearchParams.get('requestedPath') || '/').replace(/ /g, '+');
+            const requestedSearch = localStorage.getItem('requestedSearch') || '';
+            const requestedHash = localStorage.getItem('requestedHash') || '';
 
-          setFirstTimeVisitor(true);
-          localStorage.removeItem('requestedSearch');
-          localStorage.removeItem('requestedHash');
-          history.push(requestedPath + requestedSearch + requestedHash);
-        };
+            setFirstTimeVisitor(true);
+            localStorage.removeItem('requestedSearch');
+            localStorage.removeItem('requestedHash');
+            history.push(requestedPath + requestedSearch + requestedHash);
+          };
 
-        const user: User = { email: fusebitProfile?.email, ...auth0Profile, ...company };
-        const issuedByAuth0 = decoded.iss.startsWith(process.env.REACT_APP_AUTH0_DOMAIN as string);
-        getAnalyticsClient(user, issuedByAuth0);
-        trackAuthEvent(user, fusebitProfile, isSignUpEvent, navigatePostAuth);
-      });
+          const user: User = { email: fusebitProfile?.email, ...auth0Profile, ...company };
+          const issuedByAuth0 = decoded.iss.startsWith(process.env.REACT_APP_AUTH0_DOMAIN as string);
+          getAnalyticsClient(user, issuedByAuth0);
+          trackAuthEvent(user, fusebitProfile, isSignUpEvent, navigatePostAuth);
+        })
+        .catch((err) => {
+          handleError(err);
+        });
     } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error(err);
-      history.push('/logged-out');
+      handleError(err);
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
