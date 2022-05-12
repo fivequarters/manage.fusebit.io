d3d05d56acedd074c757c7963a96d66866754715
Martin Fuster <mfuster@litebox.ai>
2022-05-12 13:55:53 -0300

    complete basic functionality

--- src/components/common/MainUserInfo/MainUserInfo.tsx
+++ src/components/common/MainUserInfo/MainUserInfo.tsx
@@ -1,9 +1,14 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Auth0Token, FusebitProfile } from '@interfaces/auth0Token';
+import { Menu } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
-import accountImg from '../../../assets/account.svg';
-import rightArrow from '../../../assets/arrow-right-black.svg';
-import { useAuthContext } from '../../../hooks/useAuthContext';
-import { useGetRedirectLink } from '../../../hooks/useGetRedirectLink';
+import jwt_decode from 'jwt-decode';
+import { useLoader } from '@hooks/useLoader';
+import accountImg from '@assets/account.svg';
+import rightArrow from '@assets/arrow-right-black.svg';
+import { useAuthContext } from '@hooks/useAuthContext';
+import { useGetRedirectLink } from '@hooks/useGetRedirectLink';
 
 const StyledUserDropdownInfo = styled.div`
   display: flex;
@@ -92,13 +97,62 @@ const StyledUserDropdownStatusArrow = styled.img`
   margin-left: auto;
 `;
 
+const StyledMenu = styled(Menu)`
+  .MuiPaper-root {
+    transform: translateX(calc(-100%) - 20px) !important;
+  }
+`;
+
+const StyledAccountsWrapper = styled.div`
+  position: relative;
+  padding: 10px 15px;
+`;
+
+const StyledAccWrapper = styled.div<{ active: boolean }>`
+  margin: 8px 0;
+  padding: 10px;
+  background: ${(props) => props.active && 'var(--secondary-color)'};
+  transition: all 0.25s linear;
+
+  &:hover {
+    cursor: pointer;
+    background: var(--secondary-color);
+  }
+`;
+
 const MainUserInfo = () => {
-  const { userData } = useAuthContext();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [accounts, setAccount] = useState<FusebitProfile[]>();
+  const { userData, authUser } = useAuthContext();
   const { getRedirectLink } = useGetRedirectLink();
+  const { createLoader, removeLoader } = useLoader();
   const history = useHistory();
+
+  useEffect(() => {
+    if (userData.token) {
+      const decoded = jwt_decode<Auth0Token>(userData.token || '');
+      const fusebitProfile = decoded['https://fusebit.io/profile'];
+      setAccount(fusebitProfile.accounts);
+    }
+  }, [userData]);
 
   const handleOnClickEmail = () => {
     history.push(getRedirectLink(`/authentication/${userData.userId}/overview`));
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleAccountSwitch = (acc: FusebitProfile) => {
+    createLoader();
+    authUser(userData.token || '', acc, () => {
+      removeLoader();
+    });
   };
 
   return (
@@ -112,15 +166,41 @@ const MainUserInfo = () => {
           <StyledUserDropdownInfoEmail>{userData.primaryEmail}</StyledUserDropdownInfoEmail>
         </StyledUserDropdownPersonalInfo>
       </StyledUserDropdownInfo>
-      <Link to={getRedirectLink('/integrations/overview')}>
-        <StyledUserDropdownStatus>
-          <div>
-            <StyledUserDropdownStatusTitle>{process.env.REACT_APP_DEPLOYMENT_KEY}</StyledUserDropdownStatusTitle>
-            <StyledUserDropdownStatusId>{userData.subscriptionId}</StyledUserDropdownStatusId>
-          </div>
-          <StyledUserDropdownStatusArrow src={rightArrow} alt="right arrow" height="12" width="12" />
-        </StyledUserDropdownStatus>
-      </Link>
+      <StyledUserDropdownStatus onClick={handleClick}>
+        <div>
+          <StyledUserDropdownStatusTitle>{process.env.REACT_APP_DEPLOYMENT_KEY}</StyledUserDropdownStatusTitle>
+          <StyledUserDropdownStatusId>{userData.subscriptionId}</StyledUserDropdownStatusId>
+        </div>
+        <StyledUserDropdownStatusArrow src={rightArrow} alt="right arrow" height="12" width="12" />
+      </StyledUserDropdownStatus>
+      <StyledMenu
+        PaperProps={{
+          style: {
+            transform: anchorEl ? 'translateX(calc(-100% - 50px))' : '',
+          },
+        }}
+        id="accounts"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledAccountsWrapper>
+          {accounts?.map((acc, i) => {
+            return (
+              <StyledAccWrapper
+                onClick={() => handleAccountSwitch(acc)}
+                key={acc.userId}
+                active={userData.accountId === acc.accountId && userData.subscriptionId === acc.subscriptionId}
+              >
+                <div>Account {i}</div>
+                <div>{acc.accountId}</div>
+                <div>{acc.subscriptionId}</div>
+              </StyledAccWrapper>
+            );
+          })}
+        </StyledAccountsWrapper>
+      </StyledMenu>
     </>
   );
 };
--- src/hooks/useAuthContext.ts
+++ src/hooks/useAuthContext.ts
@@ -1,8 +1,15 @@
 import { User } from '@interfaces/user';
-import { getAnalyticsClient } from '@utils/analytics';
+import { getAnalyticsClient, trackAuthEvent } from '@utils/analytics';
 import constate from 'constate';
 import { useState } from 'react';
 import { STATIC_TENANT_ID } from '@utils/constants';
+import { Auth0Profile } from '@interfaces/auth0Profile';
+import { Company } from '@interfaces/company';
+import { createAxiosClient } from '@utils/utils';
+import { Auth0Token, FusebitProfile, FusebitProfileEx } from '@interfaces/auth0Token';
+import jwt_decode from 'jwt-decode';
+import { useHistory } from 'react-router-dom';
+import useFirstTimeVisitor from './useFirstTimeVisitor';
 
 const {
   REACT_APP_AUTH0_DOMAIN,
@@ -10,6 +17,9 @@ const {
   REACT_APP_AUTH0_CLIENT_ID,
   REACT_APP_FUSEBIT_DEPLOYMENT,
   REACT_APP_LOGOUT_REDIRECT_URL,
+  REACT_APP_FUSEBIT_USER_ID,
+  REACT_APP_FUSEBIT_ACCOUNT_ID,
+  REACT_APP_FUSEBIT_SUBSCRIPTION_ID,
 } = process.env;
 
 enum AuthStatus {
@@ -78,6 +88,8 @@ const signIn = (silent?: boolean): void => {
 const _useAuthContext = () => {
   const [authStatus, setAuthStatus] = useState<AuthStatus>(AuthStatus.UNKNOWN);
   const [userData, setUserData] = useState<User>({});
+  const { setFirstTimeVisitor } = useFirstTimeVisitor();
+  const history = useHistory();
 
   const checkAuthStatus = async () => {
     const isUserDataDefined = userData && Object.entries(userData).length > 0;
@@ -90,6 +102,101 @@ const _useAuthContext = () => {
 
   const getTenantId = () => userData.userId || STATIC_TENANT_ID;
 
+  const getAuth0ProfileAndCompany = async (auth0Token: string, accountId: string) => {
+    let auth0Profile = {} as Auth0Profile;
+    let company = {} as Company;
+    const skipXUserAgent = true;
+    const auth0AxiosClient = createAxiosClient(auth0Token, skipXUserAgent);
+    const { data: userInfo } = await auth0AxiosClient.get(`${REACT_APP_AUTH0_DOMAIN}/userinfo`);
+    auth0Profile = userInfo;
+    const fusebitAxiosClient = createAxiosClient(auth0Token);
+    const { data: account } = await fusebitAxiosClient.get(`${REACT_APP_FUSEBIT_DEPLOYMENT}/v1/account/${accountId}`);
+    company = account;
+
+    return {
+      auth0Profile,
+      company,
+    };
+  };
+
+  const redeemInitToken = async (accessToken: string, fusebitProfileEx: FusebitProfileEx, initToken: string) => {
+    const skipXUserAgent = true;
+    const client = createAxiosClient(undefined, skipXUserAgent);
+    const { data } = await client.post(`${fusebitProfileEx.fusebitProvisionUrl}/init`, {
+      initToken,
+      accessToken,
+    });
+    return data;
+  };
+
+  const handleAuthError = (error: any) => {
+    history.push(`/logged-out?error=${error}`);
+  };
+
+  const authUser = (token: string, profile?: FusebitProfile, onAuthFinished?: () => void) => {
+    const decoded = jwt_decode<Auth0Token>(token);
+    const fusebitProfile = profile || decoded['https://fusebit.io/profile'];
+    const isSignUpEvent = decoded['https://fusebit.io/new-user'] === true;
+    const initToken = window.localStorage.getItem('fusebitInitToken');
+
+    if (initToken) {
+      // This is completion of the invitation transaction. Call the provisioning function to
+      // redeem the init token, then redirect back to the home page to re-authenticate and
+      // get a new access token with an updated Fusebit profile.
+      window.localStorage.removeItem('fusebitInitToken');
+      redeemInitToken(token, fusebitProfile, initToken)
+        .then(() => {
+          history.push('/');
+        })
+        .catch((err) => {
+          handleAuthError(err);
+        });
+      return;
+    }
+
+    getAuth0ProfileAndCompany(token, fusebitProfile?.accountId || '')
+      .then(({ auth0Profile, company }) => {
+        const normalizedData = {
+          firstName: auth0Profile?.given_name || '',
+          lastName: auth0Profile?.family_name || '',
+          company: company?.displayName || '',
+        };
+
+        // for local testing: https://docs.google.com/document/d/1dkI4UdRgaD840HWc-sGi6_qz4JY8AHts97MD-1O4SpY/edit#heading=h.gtoda0wgke4n
+        if (REACT_APP_FUSEBIT_ACCOUNT_ID && REACT_APP_FUSEBIT_SUBSCRIPTION_ID && REACT_APP_FUSEBIT_USER_ID) {
+          fusebitProfile.accountId = REACT_APP_FUSEBIT_ACCOUNT_ID;
+          fusebitProfile.subscriptionId = REACT_APP_FUSEBIT_SUBSCRIPTION_ID;
+          fusebitProfile.userId = REACT_APP_FUSEBIT_USER_ID;
+        }
+
+        setUserData({ token, ...fusebitProfile, ...auth0Profile, ...company, ...normalizedData });
+        setAuthStatus(AuthStatus.AUTHENTICATED);
+
+        const navigatePostAuth = () => {
+          const urlSearchParams = new URLSearchParams(window.location.search);
+          const requestedPath = (urlSearchParams.get('requestedPath') || '/').replace(/ /g, '+');
+          const requestedSearch = localStorage.getItem('requestedSearch') || '';
+          const requestedHash = localStorage.getItem('requestedHash') || '';
+
+          setFirstTimeVisitor(true);
+          localStorage.removeItem('requestedSearch');
+          localStorage.removeItem('requestedHash');
+          history.push(requestedPath + requestedSearch + requestedHash);
+        };
+
+        const user: User = { email: fusebitProfile?.email, ...auth0Profile, ...company };
+        const issuedByAuth0 = decoded.iss.startsWith(process.env.REACT_APP_AUTH0_DOMAIN as string);
+        getAnalyticsClient(user, issuedByAuth0);
+        trackAuthEvent(user, fusebitProfile, isSignUpEvent, navigatePostAuth);
+      })
+      .catch((err) => {
+        handleAuthError(err);
+      })
+      .finally(() => {
+        onAuthFinished?.();
+      });
+  };
+
   return {
     checkAuthStatus,
     userData,
@@ -97,6 +204,10 @@ const _useAuthContext = () => {
     authStatus,
     setAuthStatus,
     getTenantId,
+    getAuth0ProfileAndCompany,
+    redeemInitToken,
+    handleAuthError,
+    authUser,
   };
 };
 
