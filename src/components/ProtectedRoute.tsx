fed84d81d0d1629a5420e8091c97990745706c11
Martin Fuster <mfuster@litebox.ai>
2022-05-31 16:06:46 -0300

    clean conditional

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,6 +4,7 @@ import { AuthStatus, useAuthContext, signIn } from '@hooks/useAuthContext';
 const ProtectedRoute: FC<{ children: ReactNode }> = ({ children }) => {
   const { authStatus, checkAuthStatus } = useAuthContext();
   const isCheckingAuthStatus = useRef(false);
+  const isAuthenticated = authStatus === AuthStatus.AUTHENTICATED;
 
   useEffect(() => {
     if (authStatus === AuthStatus.UNKNOWN && !isCheckingAuthStatus.current) {
@@ -16,7 +17,7 @@ const ProtectedRoute: FC<{ children: ReactNode }> = ({ children }) => {
     }
   }, [authStatus, checkAuthStatus]);
 
-  return <>{authStatus === AuthStatus.AUTHENTICATED && children}</>;
+  return <>{isAuthenticated && children}</>;
 };
 
 export default ProtectedRoute;
