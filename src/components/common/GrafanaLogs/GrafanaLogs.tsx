036e7725a63a5b3e1b7c674be82463bda1d446c1
Martin Fuster <mfuster@litebox.ai>
2022-06-24 12:48:37 -0300

    test interval solution

--- src/components/common/GrafanaLogs/GrafanaLogs.tsx
+++ src/components/common/GrafanaLogs/GrafanaLogs.tsx
@@ -1,4 +1,5 @@
 import useGrafanaLogs from '@hooks/useGrafanaLogs';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -6,6 +7,9 @@ const StyledLogs = styled.iframe`
   position: relative;
   border-radius: 8px;
   box-shadow: 0px 1px 30px -1px rgb(52 72 123 / 10%);
+  .panel-title {
+    display: none;
+  }
 `;
 
 const DEFAULT_IFRAME_ID = 'logging';
@@ -28,7 +32,22 @@ const GrafanaLogs = ({ from, height, iframeId, integrationId }: Props) => {
     from,
   });
 
-  return <StyledLogs id={ID} title={ID} src={url} height="100%" width="100%" frameBorder="0" />;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const iframe = document.getElementById(ID) as HTMLIFrameElement;
+      if (iframe.contentWindow && window.location.host.includes('fusebit-io')) {
+        const panel = iframe.contentWindow.document?.querySelector('.panel-title') as HTMLElement;
+        if (panel) {
+          panel.style.display = 'none';
+          clearInterval(interval);
+        }
+      }
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, [ID]);
+
+  return <StyledLogs id={ID} name={ID} title={ID} src={url} height="100%" width="100%" frameBorder="0" />;
 };
 
 export default GrafanaLogs;
