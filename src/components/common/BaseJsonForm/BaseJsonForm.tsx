5139c9f0f8cfe0258e72677cc764b9babdd0666e
MikeFuster <85583835+MikeFuster@users.noreply.github.com>
2022-08-18 16:56:21 -0300

    add ajv (#456)

--- src/components/common/BaseJsonForm/BaseJsonForm.tsx
+++ src/components/common/BaseJsonForm/BaseJsonForm.tsx
@@ -1,14 +1,19 @@
 import { JsonForms, JsonFormsInitStateProps, JsonFormsReactProps } from '@jsonforms/react';
 import { materialRenderers, materialCells } from '@jsonforms/material-renderers';
 import InputWithCopy, { inputWithCopyTester } from '@components/common/FormFields/InputWithCopy/InputWithCopy';
+import { createAjv } from '@jsonforms/core';
 import { MonacoEditorControl } from '@fusebit/monaco-jsonforms';
 
 type Props = JsonFormsInitStateProps & JsonFormsReactProps;
+const ajv = createAjv({ allErrors: true, jsonPointers: false });
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('ajv-errors')(ajv);
 
 const BaseJsonForm = (props: Omit<Props, 'renderers' | 'cells'>) => {
   return (
     <JsonForms
       {...props}
+      ajv={ajv}
       cells={materialCells}
       renderers={[
         ...materialRenderers,
