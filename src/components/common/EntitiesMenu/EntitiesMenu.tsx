8a614a6a78eb92d536f5d44b3d2222bf86795470
MikeFuster <mfuster@litebox.ai>
2022-11-16 10:48:32 -0300

    rm keepmounted

--- src/components/common/EntitiesMenu/EntitiesMenu.tsx
+++ src/components/common/EntitiesMenu/EntitiesMenu.tsx
@@ -93,7 +93,6 @@ const EntitiesMenu = ({ desktop, mobile }: Props) => {
           transformOrigin={{ horizontal: 'left', vertical: 'top' }}
           getContentAnchorEl={null}
           anchorEl={desktop.anchorEl}
-          keepMounted
           open={!!desktop.anchorEl}
           onClose={desktop.onClose}
         >
